256613767a60a1b781aeefd511f57ea9db345d69
Julian Mills <julian@starlane.org>
2022-12-14 13:53:15 +0100

    change: improve syntax highlighting
    - support lowercase operators
    - added some missing operators

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import { actions, store } from './store';
 import { App } from './components/App';
 import { LoadConfig } from '$/go/main/App';
 import { loader } from '@monaco-editor/react';
+import { initializeEditor } from './util/editor';
 
 // Load existing config
 LoadConfig().then(config => {
@@ -21,55 +22,5 @@ createRoot(root).render(
 
 // Init monaco
 loader.init().then(monaco => {
-	monaco.editor.defineTheme('surrealist', {
-		base: 'vs',
-		inherit: true,
-		rules: [
-			{ token: 'keyword', foreground: '#e600a4' },
-			{ token: 'param', foreground: '#e67a15' },
-		],
-		colors: {
-			'editorLineNumber.foreground': '#9BA9C6',
-			'editorLineNumber.activeForeground': '#465671'
-		}
-	});
-
-	monaco.editor.defineTheme('surrealist-dark', {
-		base: 'vs-dark',
-		inherit: true,
-		rules: [
-			{ token: 'keyword', foreground: '#e600a4' },
-			{ token: 'param', foreground: '#e67a15' },
-		],
-		colors: {
-			'editor.background': '#1a1b1e',
-			'editorLineNumber.foreground': '#465671',
-			'editorLineNumber.activeForeground': '#9BA9C6'
-		}
-	});
-
-	monaco.languages.register({ id: 'surrealql' });
-
-	monaco.languages.setMonarchTokensProvider('surrealql', {
-		keywords: [
-			'USE', 'LET', 'BEGIN', 'CANCEL', 'COMMIT', 'IF', 'ELSE', 'SELECT', 'INSERT', 'CREATE',
-			'UPDATE', 'RELATE', 'DELETE', 'DEFINE', 'REMOVE', 'INFO', 'FROM', 'SET', 'FOR', 'NS', 'DB',
-			'TRANSACTION', 'THEN', 'END', 'WHERE', 'SPLIT', 'AT', 'GROUP', 'BY', 'ORDER', 'ASC', 'DESC',
-			'COLLATE', 'NUMERIC', 'LIMIT', 'START', 'FETCH', 'TIMEOUT', 'PARALLEL', 'CONTENT', 'RETURN',
-			'NONE', 'BEFORE', 'AFTER', 'DIFF', 'MERGE', 'PATCH', 'SCOPE', 'TABLE'
-		],
-		tokenizer: {
-			root: [
-				[/\b\w+\b/, {
-					cases: {
-						'@keywords': 'keyword',
-						'@default': 'variable'
-					}
-				}],
-				[/".*?"/, 'string'],
-				[/(\/\/|#|--)/, 'comment'],
-				[/\$\w+/, 'param']
-			]
-		}
-	});
+	initializeEditor(monaco);
 })
--- frontend/src/util/editor.ts
+++ frontend/src/util/editor.ts
@@ -1,3 +1,4 @@
+import { Monaco } from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 
 export const baseEditorConfig: editor.IStandaloneEditorConstructionOptions = {
@@ -13,4 +14,62 @@ export const baseEditorConfig: editor.IStandaloneEditorConstructionOptions = {
 	minimap: {
 		enabled: false
 	}
+}
+
+export function initializeEditor(monaco: Monaco) {
+
+	monaco.editor.defineTheme('surrealist', {
+		base: 'vs',
+		inherit: true,
+		rules: [
+			{ token: 'keyword', foreground: '#e600a4' },
+			{ token: 'param', foreground: '#e67a15' },
+		],
+		colors: {
+			'editorLineNumber.foreground': '#9BA9C6',
+			'editorLineNumber.activeForeground': '#465671'
+		}
+	});
+
+	monaco.editor.defineTheme('surrealist-dark', {
+		base: 'vs-dark',
+		inherit: true,
+		rules: [
+			{ token: 'keyword', foreground: '#e600a4' },
+			{ token: 'param', foreground: '#e67a15' },
+		],
+		colors: {
+			'editor.background': '#1a1b1e',
+			'editorLineNumber.foreground': '#465671',
+			'editorLineNumber.activeForeground': '#9BA9C6'
+		}
+	});
+
+	monaco.languages.register({ id: 'surrealql' });
+
+	monaco.languages.setMonarchTokensProvider('surrealql', {
+		ignoreCase: true,
+		keywords: [
+			'USE', 'LET', 'BEGIN', 'CANCEL', 'COMMIT', 'IF', 'ELSE', 'SELECT', 'INSERT', 'CREATE',
+			'UPDATE', 'RELATE', 'DELETE', 'DEFINE', 'REMOVE', 'INFO', 'FROM', 'SET', 'FOR', 'NS', 'DB',
+			'TRANSACTION', 'THEN', 'END', 'WHERE', 'SPLIT', 'AT', 'GROUP', 'BY', 'ORDER', 'ASC', 'DESC',
+			'COLLATE', 'NUMERIC', 'LIMIT', 'START', 'FETCH', 'TIMEOUT', 'PARALLEL', 'CONTENT', 'RETURN',
+			'NONE', 'BEFORE', 'AFTER', 'DIFF', 'MERGE', 'PATCH', 'SCOPE', 'TABLE', 'AS', 'AND', 'OR',
+			'CONTAINS', 'CONTAINSNOT', 'CONTAINSALL', 'CONTAINSANY', 'CONTAINSNONE', 'INSIDE', 'NOTINSIDE',
+			'ALLINSIDE', 'ANYINSIDE', 'NONEINSIDE', 'OUTSIDE', 'INTERSECTS'
+		],
+		tokenizer: {
+			root: [
+				[/\b\w+\b/, {
+					cases: {
+						'@keywords': 'keyword',
+						'@default': 'variable'
+					}
+				}],
+				[/".*?"/, 'string'],
+				[/(\/\/|#|--)/, 'comment'],
+				[/\$\w+/, 'param']
+			]
+		}
+	});
 }
