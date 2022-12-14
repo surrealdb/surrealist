d9001818b49e49255201b8da58e4af509f7d20dc
Beau den Heijer <beaudenheijer@gmail.com>
2022-12-14 14:57:45 +0100

    feat: added color type completion

---
+++ frontend/src/typings/mantine.d.ts
@@ -0,0 +1,9 @@
+import {Tuple, DefaultMantineColor} from "@mantine/core";
+
+type ExtendedCustomColors = "surreal" | DefaultMantineColor;
+
+declare module "@mantine/core" {
+    export interface MantineThemeColorsOverride {
+        colors: Record<ExtendedCustomColors, Tuple<string, 10>>
+    }
+}
