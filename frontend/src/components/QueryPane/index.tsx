7d33ecea75a2eaad67183aface79f9b918cbf1de
Beau den Heijer <beaudenheijer@gmail.com>
2022-12-21 13:42:50 +0100

    feat: added a commenting feature for query panel closes: #5

--- frontend/src/components/QueryPane/index.tsx
+++ frontend/src/components/QueryPane/index.tsx
@@ -1,81 +1,124 @@
 import Editor from "@monaco-editor/react";
-import { editor, KeyCode, KeyMod } from "monaco-editor";
-import { mdiDatabase } from "@mdi/js";
-import { useStable } from "~/hooks/stable";
-import { useActiveTab } from "~/hooks/tab";
-import { actions, store } from "~/store";
-import { updateConfig } from "~/util/helpers";
-import { Panel } from "../Panel";
-import { useMemo } from "react";
-import { baseEditorConfig } from "~/util/editor";
-import { useIsLight } from "~/hooks/theme";
+import {editor, KeyCode, KeyMod} from "monaco-editor";
+import {mdiDatabase} from "@mdi/js";
+import {useStable} from "~/hooks/stable";
+import {useActiveTab} from "~/hooks/tab";
+import {actions, store} from "~/store";
+import {updateConfig} from "~/util/helpers";
+import {Panel} from "../Panel";
+import {useMemo} from "react";
+import {baseEditorConfig} from "~/util/editor";
+import {useIsLight} from "~/hooks/theme";
 
 export interface QueryPaneProps {
-	isConnected: boolean;
-	onExecuteQuery: () => void;
+    isConnected: boolean;
+    onExecuteQuery: () => void;
 }
 
 export function QueryPane(props: QueryPaneProps) {
-	const activeTab = useActiveTab();
-	const isLight = useIsLight();
-	
-	if (!activeTab) {
-		throw new Error('This should not happen');
-	}
+    const activeTab = useActiveTab();
+    const isLight = useIsLight();
 
-	const setQuery = useStable((content: string|undefined) => {
-		store.dispatch(actions.updateTab({
-			id: activeTab.id,
-			query: content || ''
-		}));
+    if (!activeTab) {
+        throw new Error('This should not happen');
+    }
 
-		updateConfig();
-	});
+    const setQuery = useStable((content: string | undefined) => {
+        store.dispatch(actions.updateTab({
+            id: activeTab.id,
+            query: content || ''
+        }));
 
-	const setEditor = useStable((editor: editor.IStandaloneCodeEditor) => {
-		editor.addAction({
-			id: 'run-query',
-			label: 'Run Query',
-			keybindings: [
-				KeyMod.CtrlCmd | KeyCode.Enter
-			],
-			run: () => {
-				props.onExecuteQuery();
-			}
-		});
-	});
+        updateConfig();
+    });
 
-	const options = useMemo<editor.IStandaloneEditorConstructionOptions>(() => {
-		return {
-			...baseEditorConfig,
-			quickSuggestions: false,
-			wordBasedSuggestions: false,
-			wrappingStrategy: 'advanced',
-			wordWrap: 'on',
-		}
-	}, []);
+    const setEditor = useStable((editor: editor.IStandaloneCodeEditor) => {
+        editor.addAction({
+            id: 'run-query',
+            label: 'Run Query',
+            keybindings: [
+                KeyMod.CtrlCmd | KeyCode.Enter
+            ],
+            run: () => {
+                props.onExecuteQuery();
+            }
+        });
 
-	return (
-		<Panel
-			title="Query"
-			icon={mdiDatabase}
-		>
-			<div
-				style={{
-					position: 'absolute',
-					insetBlock: 0,
-					insetInline: 24
-				}}
-			>
-				<Editor
-					onMount={setEditor}
-					theme={isLight ? 'surrealist' : 'surrealist-dark'}
-					value={activeTab?.query}
-					onChange={setQuery}
-					options={options}
-					language="surrealql"
-				/>
-			</div>
-		</Panel>
-	)
+        editor.addAction({
+            id: 'comment-query',
+            label: 'Comment Query',
+            keybindings: [
+                KeyMod.CtrlCmd | KeyCode.Slash
+            ],
+            run: (editor) => {
+                const selection = editor.getSelection();
+
+                if (!selection) {
+                    return;
+                }
+
+                const range = {
+                    endLineNumber: selection.endLineNumber,
+                    endColumn: selection.endColumn,
+                    startLineNumber: selection.startLineNumber,
+                    startColumn: selection.startColumn
+                }
+
+                const model = editor.getModel();
+
+                if(!model) {
+                    return;
+                }
+
+                const text = model.getValueInRange(range);
+                const lines = text.split('\n');
+
+                if(!lines) {
+                    return;
+                }
+
+                const comment = lines.filter(line => !line.startsWith('#'))
+                    .map(line => `# ${line}`).join('\n');
+
+                editor.executeEdits('comment-query', [{
+                    range,
+                    text: comment || ""
+                }]);
+            }
+        });
+    });
+
+    const options = useMemo<editor.IStandaloneEditorConstructionOptions>(() => {
+        return {
+            ...baseEditorConfig,
+            quickSuggestions: false,
+            wordBasedSuggestions: false,
+            wrappingStrategy: 'advanced',
+            wordWrap: 'on',
+        }
+    }, []);
+
+    return (
+        <Panel
+            title="Query"
+            icon={mdiDatabase}
+        >
+            <div
+                style={{
+                    position: 'absolute',
+                    insetBlock: 0,
+                    insetInline: 24
+                }}
+            >
+                <Editor
+                    onMount={setEditor}
+                    theme={isLight ? 'surrealist' : 'surrealist-dark'}
+                    value={activeTab?.query}
+                    onChange={setQuery}
+                    options={options}
+                    language="surrealql"
+                />
+            </div>
+        </Panel>
+    )
 }
