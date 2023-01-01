1fcc5a7fb84f54e5bdb689b0f1a77c2469b7def6
Julian Mills <julian.exodius@gmail.com>
2023-01-01 02:37:56 +0100

    fix: github is stupid

--- frontend/src/util/updater.tsx
+++ frontend/src/util/updater.tsx
@@ -1,19 +1,13 @@
 import { gt } from 'semver';
 import { actions, store } from '~/store';
 
-const OPTIONS = {
-	headers: {
-		'X-GitHub-Api-Version': '2022-11-28'
-	}
-}
-
 export async function runUpdateChecker() {
 	if (import.meta.env.MODE === 'development') {
 		return;
 	}
 
 	try {
-		const response = await fetch('https://api.github.com/repos/StarlaneStudios/Surrealist/releases/latest', OPTIONS);
+		const response = await fetch('https://api.github.com/repos/StarlaneStudios/Surrealist/releases/latest');
 		const result = await response.json();
 		const version = result.tag_name.slice(1);
 		const current = import.meta.env.VERSION;
