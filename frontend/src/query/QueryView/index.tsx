df6318b36e165a8305d8805c5fa8b29d36367490
Julian Mills <julian.exodius@gmail.com>
2023-02-04 19:21:25 +0100

    change: default splitter sizing

--- frontend/src/query/QueryView/index.tsx
+++ frontend/src/query/QueryView/index.tsx
@@ -1,11 +1,11 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 import { useStoreValue } from "~/store";
 import { FavoritesPane } from "../FavoritesPane";
 import { HistoryPane } from "../HistoryPane";
 import { QueryPane } from "../QueryPane";
 import { ResultPane } from "../ResultPane";
 import { VariablesPane } from "../../query/VariablesPane";
-import { Splitter } from "~/components/Splitter";
+import { Splitter, SplitValues } from "~/components/Splitter";
 
 export interface QueryViewProps {
 	isOnline: boolean;
@@ -15,10 +15,14 @@ export interface QueryViewProps {
 export function QueryView(props: QueryViewProps) {
 	const enableListing = useStoreValue(state => state.config.enableListing);
 	const queryListing = useStoreValue(state => state.config.queryListing);
+
+	const [splitValues, setSplitValues] = useState<SplitValues>([450, undefined])
 	
 	return (
 		<Splitter
 			minSize={300}
+			values={splitValues}
+			onChange={setSplitValues}
 			direction="horizontal"
 			startPane={
 				<Splitter
