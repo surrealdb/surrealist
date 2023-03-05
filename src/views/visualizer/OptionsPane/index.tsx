278de9a1759f852f1f3ff4aa15e3f4fbcbb21ba9
Julian Mills <julian.exodius@gmail.com>
2023-03-05 14:11:14 +0100

    fix: visualizer options pane cannot scroll

--- src/views/visualizer/OptionsPane/index.tsx
+++ src/views/visualizer/OptionsPane/index.tsx
@@ -1,5 +1,5 @@
 import classes from './style.module.scss';
-import { Button, Kbd, Stack, Text, Title } from "@mantine/core";
+import { Button, Kbd, ScrollArea, Stack, Text, Title } from "@mantine/core";
 import { Spacer } from "~/components/Spacer";
 import { useIsLight } from "~/hooks/theme";
 import { Panel } from "~/components/Panel";
@@ -16,58 +16,60 @@ export function OptionsPane(props: OptionsPaneProps) {
 
 	return (
 		<Panel>
-			<Stack spacing="xs" h="100%">
-				<div>
-					<Title mt="md" size={16} color={isLight ? 'light.6' : 'white'}>
-						Database Visualizer
-					</Title>
-
-					<Text color={isLight ? 'light.7' : 'light.3'} mt="sm">
-						The database visualizer view allows you to plot your tables and edges in a graph view.
-						You can use the Designer view to modify your schema and preview the changes in the visualizer.
-
-						<Title mt="md" size={14} color={isLight ? 'light.6' : 'light.0'}>
-							How it works
+			<ScrollArea className={classes.scrollArea}>
+				<Stack spacing="xs" h="100%">
+					<div>
+						<Title mt="md" size={16} color={isLight ? 'light.6' : 'white'}>
+							Database Visualizer
 						</Title>
-					
-						<ul className={classes.list}>
-							<li>
-								Edges are inferred from the schema fields <Kbd>type</Kbd> values
-							</li>
-							<li>
-								If there are multiple edges between two tables, only one is rendered.
-							</li>
-							<li>
-								A best attempt is made to position the nodes in a readable way.
-							</li>
-							<li>
-								Press <Text span color={isLight ? 'black' : 'light.0'}>Visualize</Text> to generate a graph of the current database schema.
-							</li>
-							<li>
-								Press <Text span color={isLight ? 'black' : 'light.0'}>Save snapshot</Text> to save the current graph as an image.
-							</li>
-						</ul>
-					</Text>
-				</div>
 
-				<Spacer />
+						<Text color={isLight ? 'light.7' : 'light.3'} mt="sm">
+							The database visualizer view allows you to plot your tables and edges in a graph view.
+							You can use the Designer view to modify your schema and preview the changes in the visualizer.
 
-				<Button
-					color="light"
-					variant="light"
-					onClick={props.onSnapshot}
-					disabled={!props.isOnline || !props.canSnapshot}
-				>
-					Save snapshot
-				</Button>
+							<Title mt="md" size={14} color={isLight ? 'light.6' : 'light.0'}>
+								How it works
+							</Title>
+						
+							<ul className={classes.list}>
+								<li>
+									Edges are inferred from the schema fields <Kbd>type</Kbd> values
+								</li>
+								<li>
+									If there are multiple edges between two tables, only one is rendered.
+								</li>
+								<li>
+									A best attempt is made to position the nodes in a readable way.
+								</li>
+								<li>
+									Press <Text span color={isLight ? 'black' : 'light.0'}>Visualize</Text> to generate a graph of the current database schema.
+								</li>
+								<li>
+									Press <Text span color={isLight ? 'black' : 'light.0'}>Save snapshot</Text> to save the current graph as an image.
+								</li>
+							</ul>
+						</Text>
+					</div>
 
-				<Button
-					onClick={props.onGenerate}
-					disabled={!props.isOnline}
-				>
-					Visualize
-				</Button>
-			</Stack>
+					<Spacer />
+
+					<Button
+						color="light"
+						variant="light"
+						onClick={props.onSnapshot}
+						disabled={!props.isOnline || !props.canSnapshot}
+					>
+						Save snapshot
+					</Button>
+
+					<Button
+						onClick={props.onGenerate}
+						disabled={!props.isOnline}
+					>
+						Visualize
+					</Button>
+				</Stack>
+			</ScrollArea>
 		</Panel>
 	)
 }
