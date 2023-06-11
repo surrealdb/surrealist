d49031f9d8d4d3c07dd01116bfe46bcad0b4efd5
Bibaswan Bhawal <bibaswan3600@gmail.com>
2023-06-11 13:28:17 -0700

    (fix): Updated surre

--- src/views/authentication/AccountsPane/index.tsx
+++ src/views/authentication/AccountsPane/index.tsx
@@ -1,16 +1,28 @@
-import { ActionIcon, Button, Center, Group, Menu, Modal, PasswordInput, Stack, Text, TextInput, Title } from "@mantine/core";
-import { useInputState } from "@mantine/hooks";
-import { mdiDelete, mdiDotsVertical, mdiKeyVariant, mdiLock, mdiPlus, mdiRefresh } from "@mdi/js";
-import { useEffect, useState } from "react";
-import { Form } from "~/components/Form";
-import { Icon } from "~/components/Icon";
-import { Panel } from "~/components/Panel";
-import { Spacer } from "~/components/Spacer";
-import { useIsConnected } from "~/hooks/connection";
-import { useStable } from "~/hooks/stable";
-import { useIsLight } from "~/hooks/theme";
-import { getActiveSurreal } from "~/surreal";
-import { showError } from "~/util/helpers";
+import {
+	ActionIcon,
+	Button,
+	Center,
+	Group,
+	Menu,
+	Modal,
+	PasswordInput,
+	Stack,
+	Text,
+	TextInput,
+	Title,
+} from '@mantine/core';
+import { useInputState } from '@mantine/hooks';
+import { mdiDelete, mdiDotsVertical, mdiKeyVariant, mdiLock, mdiPlus, mdiRefresh } from '@mdi/js';
+import { useEffect, useState } from 'react';
+import { Form } from '~/components/Form';
+import { Icon } from '~/components/Icon';
+import { Panel } from '~/components/Panel';
+import { Spacer } from '~/components/Spacer';
+import { useIsConnected } from '~/hooks/connection';
+import { useStable } from '~/hooks/stable';
+import { useIsLight } from '~/hooks/theme';
+import { getActiveSurreal } from '~/surreal';
+import { showError } from '~/util/helpers';
 
 export interface AccountsPaneProps {
 	icon: string;
@@ -18,7 +30,6 @@ export interface AccountsPaneProps {
 	iconColor: string;
 	typeShort: string;
 	typeLong: string;
-	field: string;
 }
 
 export function AccountsPane(props: AccountsPaneProps) {
@@ -39,7 +50,7 @@ export function AccountsPane(props: AccountsPaneProps) {
 			return [];
 		}
 
-		setLogins(Object.keys(result[props.field]));
+		setLogins(Object.keys(result.logins));
 	});
 
 	useEffect(() => {
@@ -60,7 +71,7 @@ export function AccountsPane(props: AccountsPaneProps) {
 
 			await getActiveSurreal().query(`DEFINE LOGIN ${userName} ON ${props.typeLong} PASSWORD "${editingPassword}"`);
 			await fetchLogins();
-		} catch(err: any) {
+		} catch (err: any) {
 			showError('Failed to save account', err.message);
 		}
 	});
@@ -71,7 +82,7 @@ export function AccountsPane(props: AccountsPaneProps) {
 		setEditingUsername('');
 		setEditingPassword('');
 	});
-	
+
 	const changePassword = useStable((login: string) => {
 		setIsEditing(true);
 		setEditingLogin(login);
@@ -93,72 +104,40 @@ export function AccountsPane(props: AccountsPaneProps) {
 			title={props.title}
 			rightSection={
 				<Group noWrap>
-					<ActionIcon
-						title="Add account"
-						onClick={createAccount}
-					>
-						<Icon color="light.4" path={mdiPlus} />
+					<ActionIcon title='Add account' onClick={createAccount}>
+						<Icon color='light.4' path={mdiPlus} />
 					</ActionIcon>
-					<ActionIcon
-						title="Refresh"
-						onClick={fetchLogins}
-					>
-						<Icon color="light.4" path={mdiRefresh} />
+					<ActionIcon title='Refresh' onClick={fetchLogins}>
+						<Icon color='light.4' path={mdiRefresh} />
 					</ActionIcon>
 				</Group>
-			}
-		>
+			}>
 			{logins.length === 0 && (
-				<Center h="100%" c="light.5">
+				<Center h='100%' c='light.5'>
 					No {props.title.toLocaleLowerCase()} found
 				</Center>
 			)}
 
 			<Stack spacing={0}>
 				{logins.map((login) => (
-					<Group
-						key={login}
-						spacing="xs"
-						w="100%"
-						noWrap
-					>
-						<Icon
-							color={props.iconColor}
-							path={mdiKeyVariant}
-							size={0.85}
-						/>
+					<Group key={login} spacing='xs' w='100%' noWrap>
+						<Icon color={props.iconColor} path={mdiKeyVariant} size={0.85} />
 
-						<Text color={isLight ? 'gray.9' : 'gray.0'}>
-							{login}
-						</Text>
+						<Text color={isLight ? 'gray.9' : 'gray.0'}>{login}</Text>
 						<Spacer />
-						<Menu
-							position="right-start"
-							shadow="sm"
-							withArrow
-							arrowOffset={18}
-						>
+						<Menu position='right-start' shadow='sm' withArrow arrowOffset={18}>
 							<Menu.Target>
-								<Button
-									size="xs"
-									px={5}
-									color="dark"
-									variant="subtle"
-								>
-									<Icon path={mdiDotsVertical}/>
+								<Button size='xs' px={5} color='dark' variant='subtle'>
+									<Icon path={mdiDotsVertical} />
 								</Button>
 							</Menu.Target>
 							<Menu.Dropdown>
 								<Menu.Item
-									icon={<Icon path={mdiLock} size={0.7} color="yellow.6" />}
-									onClick={() => changePassword(login)}
-								>
+									icon={<Icon path={mdiLock} size={0.7} color='yellow.6' />}
+									onClick={() => changePassword(login)}>
 									Change password
 								</Menu.Item>
-								<Menu.Item
-									icon={<Icon path={mdiDelete} size={0.7} color="red" />}
-									onClick={() => removeAccount(login)}
-								>
+								<Menu.Item icon={<Icon path={mdiDelete} size={0.7} color='red' />} onClick={() => removeAccount(login)}>
 									Remove
 								</Menu.Item>
 							</Menu.Dropdown>
@@ -175,17 +154,11 @@ export function AccountsPane(props: AccountsPaneProps) {
 					<Title size={16} color={isLight ? 'light.6' : 'white'}>
 						{editingLogin ? `Change password` : 'Create account'}
 					</Title>
-				}
-			>
+				}>
 				<Form onSubmit={saveAccount}>
 					<Stack>
 						{!editingLogin && (
-							<TextInput
-								placeholder="Enter username"
-								value={editingUsername}
-								onChange={setEditingUsername}
-								autoFocus
-							/>
+							<TextInput placeholder='Enter username' value={editingUsername} onChange={setEditingUsername} autoFocus />
 						)}
 						<PasswordInput
 							placeholder={editingLogin ? 'Enter new password' : 'Enter password'}
@@ -194,25 +167,20 @@ export function AccountsPane(props: AccountsPaneProps) {
 							autoFocus={!!editingLogin}
 						/>
 					</Stack>
-					<Group mt="lg">
-						<Button
-							onClick={closeModal}
-							color={isLight ? 'light.5' : 'light.3'}
-							variant="light"
-						>
+					<Group mt='lg'>
+						<Button onClick={closeModal} color={isLight ? 'light.5' : 'light.3'} variant='light'>
 							Close
 						</Button>
 						<Spacer />
 						<Button
-							color="surreal"
-							disabled={editingLogin ? (!editingPassword) : (!editingUsername || !editingPassword)}
-							type="submit"
-						>
+							color='surreal'
+							disabled={editingLogin ? !editingPassword : !editingUsername || !editingPassword}
+							type='submit'>
 							Save
 						</Button>
 					</Group>
 				</Form>
 			</Modal>
 		</Panel>
-	)
+	);
 }
