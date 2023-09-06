b68d18af4a290d8b0bfb7ac9509bdb63d8d8cdc1
Julian Mills <julian.exodius@gmail.com>
2023-09-06 12:52:57 +0200

    refactor: simplify query logic

--- src-tauri/src/query.rs
+++ src-tauri/src/query.rs
@@ -139,7 +139,7 @@ pub async fn execute_query(
 
     let query_task = client.query(query);
     let timeout_duration = Duration::from_secs(max_time);
-    let timeout_result = timeout(timeout_duration, async { query_task.await }).await;
+    let timeout_result = timeout(timeout_duration, query_task).await;
 
     println!("Query task completed");
 
