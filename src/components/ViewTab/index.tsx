2590cd647d933fcfd694db87161d3da58ad3498e
Julian Mills <julian.exodius@gmail.com>
2023-09-21 10:01:39 +0200

    change: remove invisible icon

--- src/components/ViewTab/index.tsx
+++ src/components/ViewTab/index.tsx
@@ -5,9 +5,6 @@ import { actions, store, useStoreValue } from "~/store";
 import { useStable } from "~/hooks/stable";
 import { updateTitle, updateConfig } from "~/util/helpers";
 import { SurrealistTab } from "~/types";
-import { VIEW_MODES } from "~/constants";
-import { Icon } from "../Icon";
-
 export interface ViewTabProps {
 	tabInfo: SurrealistTab;
 }
@@ -28,7 +25,6 @@ export function ViewTab({ tabInfo }: PropsWithChildren<ViewTabProps>) {
 
 	return (
 		<Button px="md" pt={1} c={fgColor} color={bgColor} variant={isActive ? "filled" : "subtle"} onClick={select}>
-			<Icon path={VIEW_MODES.find((v) => v.id === tabInfo?.activeView)!.icon} size={0.9} left />
 			{tabInfo?.name}
 		</Button>
 	);
