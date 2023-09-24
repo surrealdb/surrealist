ec4dbbc26c66060d7ce06a11352fd572073de938
Julian Mills <julian.exodius@gmail.com>
2023-09-24 13:34:23 +0200

    change: use new config file location

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -2,56 +2,75 @@ use std::{
     env,
     fs::{self, File},
     io::{Read, Write},
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
-use tauri::api::path::home_dir;
+use tauri::api::path::{home_dir, config_dir};
 
 const DEFAULT_CONFIG: &str = "{}";
 
-fn get_config_path() -> String {
+fn get_legacy_config_path() -> PathBuf {
     let mut base_dir = home_dir().unwrap_or_else(|| PathBuf::from("."));
 
     base_dir.push(".surrealist.json");
+
     if base_dir.exists() {
-        return base_dir.to_str().unwrap().to_string();
+        return base_dir;
     }
 
     let config_dir_result = env::var("XDG_CONFIG_HOME");
+
     return match config_dir_result {
         Ok(value) => {
             let mut config = PathBuf::from(value);
             config.push("surrealist");
             config.push("config.json");
-            config.to_str().unwrap().to_string()
+            config
         }
-        Err(_) => base_dir.to_str().unwrap().to_string(),
+        Err(_) => base_dir
     };
+}
+
+fn get_config_path() -> PathBuf {
+    let mut config_path = config_dir().expect("Config directory should be resolvable");
+
+    config_path.push("surrealist.json");
+    config_path
 }
 
 #[tauri::command]
 pub fn load_config() -> String {
-    let read_op = File::open(get_config_path());
-    let mut result = String::new();
+	let legacy_path = get_legacy_config_path();
+	let config_path = get_config_path();
+
+	// Migrate the legacy config
+	if legacy_path.exists() {
+		fs::rename(&legacy_path, &config_path).expect("config to be migrated");
+	}
+
+	// Attempt to read the config file
+	let read_op = File::open(&config_path);
+    let mut buffer = String::new();
 
     match read_op {
         Ok(mut file) => {
-            file.read_to_string(&mut result)
+            file.read_to_string(&mut buffer)
                 .expect("config should be readable");
         }
         Err(_) => {
             save_config(DEFAULT_CONFIG);
-            result = DEFAULT_CONFIG.to_string();
+            buffer = DEFAULT_CONFIG.to_string();
         }
     }
 
-    result
+    buffer
 }
 
 #[tauri::command]
 pub fn save_config(config: &str) {
-    let path_string = get_config_path();
-    let path = Path::new(&path_string);
+    let config_path = get_config_path();
+    let path = PathBuf::from(config_path);
     let parent = path.parent().unwrap();
+
     fs::create_dir_all(parent).expect("config directory should be writable");
 
     let mut write_op = File::create(get_config_path()).unwrap();
