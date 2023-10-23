ca939079b823a6b5e81319ae92091672028b946a
Julian Mills <julian.exodius@gmail.com>
2023-10-24 00:21:00 +0200

    fix: improved github pages support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
-import { readFileSync } from 'node:fs';
+import { readFileSync, cpSync } from 'node:fs';
 
 const { version, author, surreal } = JSON.parse(readFileSync('./package.json', 'utf8'));
 
+function pages(): PluginOption {
+	return {
+		name: 'github-pages',
+
+		writeBundle() {
+			const from = fileURLToPath(new URL('dist/index.html', import.meta.url));
+			const to = fileURLToPath(new URL('dist/404.html', import.meta.url));
+
+			cpSync(from, to);
+		}
+	};
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), pages()],
 	clearScreen: false,
 	envPrefix: ['VITE_', 'TAURI_'],
 	server: {
