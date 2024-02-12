da175d0c41e540df6ae06926c73794807c4e4dd1
Micha de Vries <micha@devrie.sh>
2024-02-12 18:32:55 +0100

    Add dev deploy workflow

---
+++ aws/prod/viewer-request/index.js
@@ -0,0 +1,34 @@
+function handler(event) {
+
+	let request = event.request;
+	let host = request.headers.host.value;
+
+	if (host !== 'dev.surrealist.app') {
+
+		return {
+			statusCode: 301,
+			statusDescription: 'Moved Permanently',
+			headers: {
+				location: {
+					value: `https://dev.surrealist.app${path}`
+				}
+			},
+		};
+
+	}
+
+	switch (true) {
+		case request.uri === '/embed/new':
+			request.uri = '/embed/new.html';
+			return request;
+		case request.uri === '/embed':
+			request.uri = '/embed/run.html';
+			return request;
+		case request.uri.includes('.') === false:
+			request.uri = '/index.html';
+			return request;
+	}
+
+	return request;
+
+}
