6a3d42d81a58510d41fd594351d71b590540abb3
Julian Mills <julian.exodius@gmail.com>
2024-02-24 02:47:27 +0100

    feat: query dragging

--- src/views/query/TabsPane/index.tsx
+++ src/views/query/TabsPane/index.tsx
@@ -3,7 +3,6 @@ import { ActionIcon, Badge, Button, Divider, ScrollArea, Stack, Text } from "@ma
 import { EditableText } from "~/components/EditableText";
 import { Icon } from "~/components/Icon";
 import { ContentPane } from "~/components/Pane";
-import { Spacer } from "~/components/Spacer";
 import { useActiveConnection } from "~/hooks/connection";
 import { useStable } from "~/hooks/stable";
 import { useIsLight } from "~/hooks/theme";
@@ -11,10 +10,11 @@ import { useConfigStore } from "~/stores/config";
 import { useInterfaceStore } from "~/stores/interface";
 import { LiveIndicator } from "~/components/LiveIndicator";
 import { getSurreal } from "~/util/surreal";
-import { iconClose, iconCopy, iconHistory, iconList, iconPlus, iconQuery, iconStar } from "~/util/icons";
+import { iconArrowUpRight, iconClose, iconCopy, iconHistory, iconList, iconPlus, iconQuery, iconStar } from "~/util/icons";
 import { Entry } from "~/components/Entry";
 import { useContextMenu } from "mantine-contextmenu";
 import { TabQuery } from "~/types";
+import { Sortable } from "~/components/Sortable";
 
 export interface TabsPaneProps {
 	openHistory: () => void;
@@ -22,7 +22,7 @@ export interface TabsPaneProps {
 }
 
 export function TabsPane(props: TabsPaneProps) {
-	const { addQueryTab, removeQueryTab, updateQueryTab, setActiveQueryTab } = useConfigStore.getState();
+	const { updateCurrentConnection, addQueryTab, removeQueryTab, updateQueryTab, setActiveQueryTab } = useConfigStore.getState();
 	const { queries, activeQuery } = useActiveConnection();
 	const { showContextMenu } = useContextMenu();
 	const liveTabs = useInterfaceStore((s) => s.liveTabs);
@@ -42,6 +42,12 @@ export function TabsPane(props: TabsPaneProps) {
 		updateQueryTab({
 			id,
 			name
+		});
+	});
+
+	const saveQueryOrder = useStable((queries: TabQuery[]) => {
+		updateCurrentConnection({
+			queries
 		});
 	});
 
@@ -81,67 +87,80 @@ export function TabsPane(props: TabsPaneProps) {
 				bottom={12}
 				gap={0}
 			>
-				<ScrollArea>
+				<ScrollArea flex={1}>
 					<Stack gap="xs" pb="md">
-						{queries.map((query) => {
-							const isActive = query.id === activeQuery;
-							const isLive = liveTabs.has(query.id);
-
-							return (
-								<Entry
-									key={query.id}
-									isActive={isActive}
-									onClick={() => setActiveQueryTab(query.id)}
-									className={classes.query}
-									onContextMenu={showContextMenu([
-										{
-											key: "duplicate",
-											title: "Duplicate query",
-											icon: <Icon path={iconCopy} />,
-											onClick: () => duplicateQuery(query)
+						<Sortable
+							items={queries}
+							direction="vertical"
+							constraint={{ distance: 10 }}
+							onSorted={saveQueryOrder}
+						>
+							{({ item: query, handleProps }) => {
+								const isActive = query.id === activeQuery;
+								const isLive = liveTabs.has(query.id);
+	
+								return (
+									<Entry
+										key={query.id}
+										isActive={isActive}
+										onClick={() => setActiveQueryTab(query.id)}
+										className={classes.query}
+										onContextMenu={showContextMenu([
+											{
+												key: "open",
+												title: "Open query",
+												icon: <Icon path={iconArrowUpRight} />,
+												onClick: () => setActiveQueryTab(query.id)
+											},
+											{
+												key: "duplicate",
+												title: "Duplicate query",
+												icon: <Icon path={iconCopy} />,
+												onClick: () => duplicateQuery(query)
+											}
+										])}
+										leftSection={
+											<Icon path={iconQuery} />
 										}
-									])}
-									leftSection={
-										<Icon path={iconQuery} />
-									}
-									rightSection={
-										<>
-											{isLive && (
-												<LiveIndicator
-													className={classes.queryLive}
-													mr={-4}
-												/>
-											)}
-
-											{queries.length > 1 && (
-												<ActionIcon
-													component="div"
-													className={classes.queryClose}
-													onClick={(e) => removeTab(query.id, e)}
-													color={(isActive && isLight) ? "white" : undefined}
-													bg={isActive ? "rgba(255, 255, 255, 0.15)" : undefined}
-												>
-													<Icon path={iconClose} />
-												</ActionIcon>
-											)}
-										</>
-									}
-								>
-									<EditableText
-										value={query.name || ''}
-										onChange={(value) => renameQuery(query.id, value)}
-										withDoubleClick
-										withDecoration
-										style={{
-											outline: 'none',
-										}}
-									/>
-								</Entry>
-							);
-						})}
+										rightSection={
+											<>
+												{isLive && (
+													<LiveIndicator
+														className={classes.queryLive}
+														mr={-4}
+													/>
+												)}
+	
+												{queries.length > 1 && (
+													<ActionIcon
+														component="div"
+														className={classes.queryClose}
+														onClick={(e) => removeTab(query.id, e)}
+														color={(isActive && isLight) ? "white" : undefined}
+														bg={isActive ? "rgba(255, 255, 255, 0.15)" : undefined}
+													>
+														<Icon path={iconClose} />
+													</ActionIcon>
+												)}
+											</>
+										}
+										{...handleProps}
+									>
+										<EditableText
+											value={query.name || ''}
+											onChange={(value) => renameQuery(query.id, value)}
+											withDoubleClick
+											withDecoration
+											style={{
+												outline: 'none',
+											}}
+										/>
+									</Entry>
+								);
+							}}
+						</Sortable>
 					</Stack>
 				</ScrollArea>
-				<Spacer />
 				<Divider mb="xs" />
 				<Stack>
 					<Text
