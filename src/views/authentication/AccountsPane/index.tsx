674c91ef2620721dd3c051957601c5bad3a62e69
Micha de Vries <micha@devrie.sh>
2024-02-27 20:42:06 +0100

    Remove unused select (again)

--- src/views/authentication/AccountsPane/index.tsx
+++ src/views/authentication/AccountsPane/index.tsx
@@ -8,7 +8,6 @@ import {
 	Modal,
 	PasswordInput,
 	ScrollArea,
-	Select,
 	Stack,
 	Text,
 	TextInput,
@@ -229,14 +228,6 @@ export function AccountsPane(props: AccountsPaneProps) {
 							value={editingPassword}
 							onChange={setEditingPassword}
 							required={!currentUser}
-						/>
-
-						<Select
-							label="Select a role"
-							description="The role of the user on this database"
-							placeholder="Select a role"
-							data={ROLES}
-							required
 						/>
 
 						<Checkbox.Group
