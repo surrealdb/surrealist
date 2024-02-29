88a729502392ab6ca317078255bc90262a58a903
Julian Mills <julian.exodius@gmail.com>
2024-02-29 16:47:22 +0100

    change: move desktop config location

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,54 +1,28 @@
 use std::{
-    env,
     fs::{self, File},
     io::{Read, Write},
     path::PathBuf,
 };
-use tauri::api::path::{config_dir, home_dir};
+use tauri::api::path::config_dir;
 
 const DEFAULT_CONFIG: &str = "{}";
-
-fn get_legacy_config_path() -> PathBuf {
-    let mut base_dir = home_dir().unwrap_or_else(|| PathBuf::from("."));
-
-    base_dir.push(".surrealist.json");
-
-    if base_dir.exists() {
-        return base_dir;
-    }
-
-    let config_dir_result = env::var("XDG_CONFIG_HOME");
-
-    match config_dir_result {
-        Ok(value) => {
-            let mut config = PathBuf::from(value);
-            config.push("surrealist");
-            config.push("config.json");
-            config
-        }
-        Err(_) => base_dir,
-    }
-}
 
 fn get_config_path() -> PathBuf {
     let mut config_path = config_dir().expect("Config directory should be resolvable");
 
-    config_path.push("surrealist.json");
+    config_path.push("SurrealDB");
+    config_path.push("Surrealist");
+    config_path.push("config.json");
+
     config_path
 }
 
 #[tauri::command]
 pub fn load_config() -> String {
-    let legacy_path = get_legacy_config_path();
     let config_path = get_config_path();
 
-    // Migrate the legacy config
-    if legacy_path.exists() {
-        fs::rename(&legacy_path, &config_path).expect("config to be migrated");
-    }
-
     // Attempt to read the config file
-    let read_op = File::open(&config_path);
+    let read_op = File::open(config_path);
     let mut buffer = String::new();
 
     match read_op {
