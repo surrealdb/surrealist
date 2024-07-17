443da0f95adbf403a5bd05abc2e20fbc0ffebfc0
Micha de Vries <micha@devrie.sh>
2024-07-17 17:35:44 +0200

    `build:preview` to build with uncompressed wasm bundle

---
+++ vite.config.preview.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { getDefaultConfig, getDefaultPlugins } from './vite.config';
+
+// https://vitejs.dev/config/
+export default defineConfig((config) => {
+	const defaultConfig = getDefaultConfig(config);
+
+	return {
+		...defaultConfig,
+		plugins: [
+			...getDefaultPlugins(),
+		]
+	};
+});
