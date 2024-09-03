5cf8987c37ddeaccaae82cd2f2a33f4d8e6740f2
Julian Mills <julian.exodius@gmail.com>
2024-09-03 18:04:21 +0200

    change: always handle preview mode

--- vite.config.ts
+++ vite.config.ts
@@ -22,82 +22,88 @@ export const getDefaultPlugins = () => [
 	})
 ];
 
-export const getDefaultConfig = ({ mode }: { mode?: string }): UserConfig => ({
-	plugins: [
-		...getDefaultPlugins(),
-		compression({
-			deleteOriginalAssets: true,
-			include: /\.(wasm)$/,
-			filename: (id) => id,
-		})
-	],
-	clearScreen: false,
-	envPrefix: ['VITE_', 'TAURI_'],
-	server: {
-		port: 1420,
-		strictPort: true
-	},
-	build: {
-		target: "es2020",
-		minify: process.env.TAURI_DEBUG ? false : 'esbuild',
-		sourcemap: !!process.env.TAURI_DEBUG,
-		rollupOptions: {
-			input: (process.env.TAURI_ENV_PLATFORM || mode === "development") ? {
-				'surrealist': '/index.html',
-			} : {
-				'surrealist': '/index.html',
-				'mini-run': '/mini/run/index.html',
-				'mini-new': '/mini/new/index.html',
-				'cloud-manage': '/cloud/manage/index.html',
-				'cloud-callback': '/cloud/callback/index.html',
-			},
-			output: {
-				experimentalMinChunkSize: 5000,
-				manualChunks: {
-					react: ["react", "react-dom"],
-					posthog: ["posthog-js"],
-					codemirror: ["codemirror", "@surrealdb/codemirror", "@surrealdb/lezer", "@replit/codemirror-indentation-markers"],
-					mantime: ["@mantine/core", "@mantine/hooks", "@mantine/notifications"],
-					surreal: ["surrealdb", "@surrealdb/wasm", "@surrealdb/ql-wasm"]
+export function getDefaultConfig({ mode }: { mode?: string }): UserConfig {
+
+	// Required because we cannot pass a custom mode to tauri build
+	mode = isPreview ? "preview" : mode;
+
+	return {
+		plugins: [
+			...getDefaultPlugins(),
+			compression({
+				deleteOriginalAssets: true,
+				include: /\.(wasm)$/,
+				filename: (id) => id,
+			})
+		],
+		clearScreen: false,
+		envPrefix: ['VITE_', 'TAURI_'],
+		server: {
+			port: 1420,
+			strictPort: true
+		},
+		build: {
+			target: "es2020",
+			minify: process.env.TAURI_DEBUG ? false : 'esbuild',
+			sourcemap: !!process.env.TAURI_DEBUG,
+			rollupOptions: {
+				input: (process.env.TAURI_ENV_PLATFORM || mode === "development") ? {
+					'surrealist': '/index.html',
+				} : {
+					'surrealist': '/index.html',
+					'mini-run': '/mini/run/index.html',
+					'mini-new': '/mini/new/index.html',
+					'cloud-manage': '/cloud/manage/index.html',
+					'cloud-callback': '/cloud/callback/index.html',
+				},
+				output: {
+					experimentalMinChunkSize: 5000,
+					manualChunks: {
+						react: ["react", "react-dom"],
+						posthog: ["posthog-js"],
+						codemirror: ["codemirror", "@surrealdb/codemirror", "@surrealdb/lezer", "@replit/codemirror-indentation-markers"],
+						mantime: ["@mantine/core", "@mantine/hooks", "@mantine/notifications"],
+						surreal: ["surrealdb", "@surrealdb/wasm", "@surrealdb/ql-wasm"]
+					}
 				}
+			},
+		},
+		esbuild: {
+			supported: {
+				'top-level-await': true //browsers can handle top-level-await features
+			},
+		},
+		resolve: {
+			alias: {
+				'~': fileURLToPath(new URL('src', import.meta.url))
 			}
 		},
-	},
-	esbuild: {
-		supported: {
-			'top-level-await': true //browsers can handle top-level-await features
-		},
-	},
-	resolve: {
-		alias: {
-			'~': fileURLToPath(new URL('src', import.meta.url))
-		}
-	},
-	css: {
-		modules: {
-			localsConvention: 'dashesOnly'
-		},
-		preprocessorOptions: {
-			scss: {
-				additionalData: '@import "~/assets/styles/mixins.scss";',
+		css: {
+			modules: {
+				localsConvention: 'dashesOnly'
+			},
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@import "~/assets/styles/mixins.scss";',
+				},
 			},
 		},
-	},
-	define: {
-		'import.meta.env.VERSION': JSON.stringify(version),
-		'import.meta.env.SDB_VERSION': JSON.stringify(surreal),
-		'import.meta.env.POSTHOG_KEY': JSON.stringify("phc_BWVuHaJuhnFi3HthLhb9l8opktRrNeFHVnisZdQ5404"),
-		'import.meta.env.POSTHOG_URL': JSON.stringify("https://eu.i.posthog.com"),
-		'import.meta.env.MODE': JSON.stringify(isPreview ? "preview" : mode),
-	},
-	optimizeDeps: {
-		exclude: ['@surrealdb/wasm', '@surrealdb/ql-wasm'],
-		esbuildOptions: {
-			target: 'esnext',
+		define: {
+			'import.meta.env.VERSION': JSON.stringify(version),
+			'import.meta.env.SDB_VERSION': JSON.stringify(surreal),
+			'import.meta.env.POSTHOG_KEY': JSON.stringify("phc_BWVuHaJuhnFi3HthLhb9l8opktRrNeFHVnisZdQ5404"),
+			'import.meta.env.POSTHOG_URL': JSON.stringify("https://eu.i.posthog.com"),
+			'import.meta.env.MODE': JSON.stringify(mode),
 		},
-	},
-	assetsInclude: ['**/@surrealdb/wasm/dist/*.wasm', '**/@surrealdb/ql-wasm/dist/*.wasm']
-});
+		optimizeDeps: {
+			exclude: ['@surrealdb/wasm', '@surrealdb/ql-wasm'],
+			esbuildOptions: {
+				target: 'esnext',
+			},
+		},
+		assetsInclude: ['**/@surrealdb/wasm/dist/*.wasm', '**/@surrealdb/ql-wasm/dist/*.wasm']
+	};
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(getDefaultConfig);
