af3fa452ed6de868631db77b3fba970a78a3cf4f
Julian Mills <julian.exodius@gmail.com>
2024-09-05 14:41:10 +0200

    progress

--- src/screens/start/index.tsx
+++ src/screens/start/index.tsx
@@ -1,19 +1,50 @@
-import { Box, Center, Group, Stack, UnstyledButton } from "@mantine/core";
-import clsx from "clsx";
-import { adapter } from "~/adapter";
-import cloudDarkUrl from "~/assets/images/dark/start-cloud.webp";
-import connectionDarkUrl from "~/assets/images/dark/start-connection.webp";
-import sandboxDarkUrl from "~/assets/images/dark/start-sandbox.webp";
-import cloudLightUrl from "~/assets/images/light/start-cloud.webp";
-import connectionLightUrl from "~/assets/images/light/start-connection.webp";
-import sandboxLightUrl from "~/assets/images/light/start-sandbox.webp";
-import startGlow from "~/assets/images/start-glow.webp";
-import { SANDBOX } from "~/constants";
-import { useStable } from "~/hooks/stable";
-import { useThemeImage } from "~/hooks/theme";
-import { dispatchIntent } from "~/hooks/url";
-import { useConfigStore } from "~/stores/config";
 import classes from "./style.module.scss";
+import iconUrl from "~/assets/images/icon.webp";
+import logoDarkUrl from "~/assets/images/dark/logo.webp";
+import logoLightUrl from "~/assets/images/light/logo.webp";
+import { Box, Image, Paper, Stack, Text } from "@mantine/core";
+import { useConfigStore } from "~/stores/config";
+import { useStable } from "~/hooks/stable";
+import { SANDBOX } from "~/constants";
+import { adapter } from "~/adapter";
+import { dispatchIntent } from "~/hooks/url";
+import { useThemeImage } from "~/hooks/theme";
+import { Entry } from "~/components/Entry";
+import { Icon } from "~/components/Icon";
+import { iconCloud, iconCog, iconPlus, iconServer, iconSurreal } from "~/util/icons";
+
+interface StartScreenProps {
+	title: string;
+	subtitle: string;
+	icon: string;
+	onClick: () => void;
+}
+
+function StartAction({
+	title,
+	subtitle,
+	icon,
+	onClick,
+}: StartScreenProps) {
+	return (
+		<Entry
+			leftSection={<Icon path={icon} size={1.05} />}
+			onClick={onClick}
+			size="md"
+			h={52}
+		>
+			<Stack gap={3} align="start" ml="sm">
+				{title}
+				<Text
+					fz="xs"
+					c="slate.3"
+				>
+					{subtitle}
+				</Text>
+			</Stack>
+		</Entry>
+	);
+}
 
 export function StartScreen() {
 	const { setActiveConnection, setActiveScreen, setActiveView } =
@@ -32,19 +63,9 @@ export function StartScreen() {
 		setActiveView("cloud");
 	});
 
-	const connectionUrl = useThemeImage({
-		light: connectionLightUrl,
-		dark: connectionDarkUrl,
-	});
-
-	const sandboxUrl = useThemeImage({
-		light: sandboxLightUrl,
-		dark: sandboxDarkUrl,
-	});
-
-	const cloudUrl = useThemeImage({
-		light: cloudLightUrl,
-		dark: cloudDarkUrl,
+	const logoUrl = useThemeImage({
+		light: logoLightUrl,
+		dark: logoDarkUrl
 	});
 
 	return (
@@ -53,55 +74,72 @@ export function StartScreen() {
 				<Box data-tauri-drag-region className={classes.titlebar} />
 			)}
 
-			<div
-				className={classes.glow}
-				style={{
-					backgroundImage: `url(${startGlow})`,
-				}}
-			/>
+			<Stack
+				h="100%"
+				justify="center"
+				align="center"
+			>
+				<Image
+					src={iconUrl}
+					w={85}
+				/>
 
-			<Center h="100%">
-				<Group align="stretch" wrap="nowrap">
-					<Stack>
-						<UnstyledButton
-							className={classes.startBox}
-							w={320}
-							h={226}
+				<Box>
+					<Image
+						src={logoUrl}
+						w={225}
+						mt="xs"
+					/>
+
+					<Text
+						c="slate"
+						ta="center"
+						mt={6}
+					>
+						Version {import.meta.env.VERSION}
+					</Text>
+				</Box>
+
+				<Paper
+					p="lg"
+					mt={35}
+					w={300}
+					style={{ border: '1px solid rgba(255, 255, 255, 0.1)'}}
+				>
+					<Stack gap="sm">
+						<StartAction
+							title="Create connection"
+							subtitle="Connect to a remote or local database"
+							icon={iconPlus}
 							onClick={openConnectionCreator}
-						>
-							<Box
-								style={{
-									backgroundImage: `url(${connectionUrl})`,
-								}}
-							/>
-						</UnstyledButton>
-						<UnstyledButton
-							className={classes.startBox}
-							w={320}
-							h={226}
+						/>
+						<StartAction
+							title="Open the Sandbox"
+							subtitle="Explore SurrealDB right inside Surrealist"
+							icon={iconSurreal}
 							onClick={openSandbox}
-						>
-							<Box
-								style={{
-									backgroundImage: `url(${sandboxUrl})`,
-								}}
-							/>
-						</UnstyledButton>
-					</Stack>
-					<Box>
-						<UnstyledButton
-							className={clsx(classes.startBox, classes.cloudBox)}
-							w={657}
-							h={464}
+						/>
+						<StartAction
+							title="Surreal Cloud"
+							subtitle="Manage your databases in the cloud"
+							icon={iconCloud}
 							onClick={openCloud}
-						>
-							<Box
-								style={{ backgroundImage: `url(${cloudUrl})` }}
-							/>
-						</UnstyledButton>
-					</Box>
-				</Group>
-			</Center>
+						/>
+						<StartAction
+							title="List connections"
+							subtitle="Manage your existing connections"
+							icon={iconServer}
+							onClick={() => dispatchIntent("open-connections")}
+						/>
+						<StartAction
+							title="Settings"
+							subtitle="Configure Surrealist to your liking"
+							icon={iconCog}
+							onClick={() => dispatchIntent("open-settings")}
+						/>
+					</Stack>
+				</Paper>
+			</Stack>
 		</Box>
 	);
 }
