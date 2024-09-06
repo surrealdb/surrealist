51d7925202eb7bd9a6e9840808bedbddb3b5983c
Julian Mills <julian.exodius@gmail.com>
2024-09-06 14:30:46 +0200

    feat: feature flag search

--- src/components/App/settings/tabs/FeatureFlags.tsx
+++ src/components/App/settings/tabs/FeatureFlags.tsx
@@ -1,19 +1,38 @@
-import { Stack, Select, Group, ActionIcon } from "@mantine/core";
+import { Stack, Select, Group, ActionIcon, TextInput } from "@mantine/core";
 import { featureFlags, schema, useFeatureFlags } from "~/util/feature-flags";
 import { useMemo } from "react";
 import { Spacer } from "~/components/Spacer";
 import { Text } from "@mantine/core";
 import { Icon } from "~/components/Icon";
-import { iconReset } from "~/util/icons";
+import { iconReset, iconSearch } from "~/util/icons";
+import { useInputState } from "@mantine/hooks";
+import { fuzzyMatch } from "~/util/helpers";
 
 export function FeatureFlagsTab() {
 	const [flags, setFlags] = useFeatureFlags();
 	const flagNames = useMemo(() => Object.keys(flags) as (keyof typeof flags)[], [flags]);
 	const defaults = featureFlags.initialStore;
 
+	const [search, setSearch] = useInputState("");
+
+	const filteredFlags = useMemo(() => {
+		return flagNames.filter((flag) => fuzzyMatch(search, flag));
+	}, [flagNames, search]);
+
 	return (
 		<Stack gap="xs" style={{ overflow: 'hidden' }}>
-			{flagNames.map((flag) => {
+			<TextInput
+				leftSection={<Icon path={iconSearch} size="sm" />}
+				placeholder="Filter feature flags"
+				variant="unstyled"
+				value={search}
+				onChange={setSearch}
+				autoFocus
+				size="xs"
+				w="100%"
+				mb="sm"
+			/>
+			{filteredFlags.map((flag) => {
 				const mapped = Object.fromEntries(schema[flag].options.map(v => [v.toString(), v]));
 				const data = schema[flag].options.map((value) => value.toString());
 
