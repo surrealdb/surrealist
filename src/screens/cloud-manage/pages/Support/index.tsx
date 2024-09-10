16adeca7b62689908b002e623996b5b2d05a77c0
Julian Mills <julian.exodius@gmail.com>
2024-09-10 15:37:56 +0200

    change: update support page

--- src/screens/cloud-manage/pages/Support/index.tsx
+++ src/screens/cloud-manage/pages/Support/index.tsx
@@ -1,80 +1,118 @@
-import { type BoxProps, Text, UnstyledButton } from "@mantine/core";
-import { Center, Group, Paper, SimpleGrid, Stack } from "@mantine/core";
+import classes from "./style.module.scss";
+
+import discordDarkUrl from "~/assets/images/dark/picto-discord.svg";
+import discordLightUrl from "~/assets/images/light/picto-discord.svg";
+import documentationDarkUrl from "~/assets/images/dark/picto-documentation.svg";
+import documentationLightUrl from "~/assets/images/light/picto-documentation.svg";
+import communtyDarkUrl from "~/assets/images/dark/picto-community.svg";
+import communtyLightUrl from "~/assets/images/light/picto-community.svg";
+
+import { type BoxProps, Image, Text, UnstyledButton } from "@mantine/core";
+import { Center, Group, Paper, Stack } from "@mantine/core";
 import { adapter } from "~/adapter";
 import { Icon } from "~/components/Icon";
 import { PrimaryTitle } from "~/components/PrimaryTitle";
-import { iconBook, iconChat, iconDiscord, iconEmail } from "~/util/icons";
-import classes from "./style.module.scss";
+import { iconChevronRight } from "~/util/icons";
+import { useThemeImage } from "~/hooks/theme";
 
 interface SupportTileProps extends BoxProps {
-	icon: string;
+	image: string;
 	title: string;
 	onClick?: () => void;
 }
 
-function SupportTile({ icon, title, onClick, ...props }: SupportTileProps) {
+function SupportTile({ image, title, onClick, ...props }: SupportTileProps) {
 	return (
 		<UnstyledButton onClick={onClick}>
 			<Paper
-				withBorder
-				style={{ aspectRatio: 1 }}
+				p="lg"
 				className={classes.tile}
 				{...props}
 			>
-				<Stack justify="center" align="center" h="100%">
-					<Icon path={icon} size="xl" c="bright" />
-					<Text>{title}</Text>
-				</Stack>
+				<Group>
+					<Image
+						src={image}
+						w={42}
+						h={42}
+					/>
+					<Text
+						flex={1}
+						c="bright"
+						ml="sm"
+						fz="lg"
+						fw={500}
+					>
+						{title}
+					</Text>
+					<Icon
+						path={iconChevronRight}
+						c="slate"
+						size="xl"
+					/>
+				</Group>
 			</Paper>
 		</UnstyledButton>
 	);
 }
 
 export function SupportPage() {
+	const discordUrl = useThemeImage({
+		dark: discordDarkUrl,
+		light: discordLightUrl,
+	});
+
+	const documentationUrl = useThemeImage({
+		dark: documentationDarkUrl,
+		light: documentationLightUrl,
+	});
+
+	const communtyUrl = useThemeImage({
+		dark: communtyDarkUrl,
+		light: communtyLightUrl,
+	});
+
 	return (
 		<Center flex={1}>
-			<Paper p="xl" bg="transparent" component={Stack}>
-				<Group gap={35} flex={1} align="stretch">
-					<Stack flex={1} w={420}>
-						<Group>
-							<Icon path={iconEmail} />
-							<PrimaryTitle>Looking for help?</PrimaryTitle>
-						</Group>
-						<Text>
-							Running into issues with your cloud account,
-							billing, or instances? We're here to help! Choose
-							one of the following community support channels for
-							help, or to get in touch with our team.
+			<Paper
+				p="xl"
+				bg="transparent"
+				component={Stack}
+			>
+				<Group
+					gap={35}
+					flex={1}
+					align="stretch"
+				>
+					<Stack
+						flex={1}
+						w={552}
+					>
+						<PrimaryTitle>Looking for help?</PrimaryTitle>
+						<Text
+							mb="xl"
+							fz="lg"
+						>
+							Running into issues with your cloud account, billing, or instances?
+							We're here to help! Reach out to us through one of the following
+							community support channels for help, or to get in touch with our team.
 						</Text>
-						<SimpleGrid cols={3} mt="md">
-							<SupportTile
-								icon={iconDiscord}
-								title="Discord"
-								onClick={() =>
-									adapter.openUrl(
-										"https://discord.gg/dc4JNWrrMc",
-									)
-								}
-							/>
-							<SupportTile
-								icon={iconBook}
-								title="Documentation"
-								onClick={() =>
-									adapter.openUrl(
-										"https://surrealdb.com/docs/cloud",
-									)
-								}
-							/>
-							<SupportTile
-								icon={iconChat}
-								title="Discourse"
-								onClick={() =>
-									adapter.openUrl(
-										"https://surrealdb.com/community/forums",
-									)
-								}
-							/>
-						</SimpleGrid>
+						<SupportTile
+							image={discordUrl}
+							title="Discord Chat"
+							onClick={() => adapter.openUrl("https://discord.gg/dc4JNWrrMc")}
+						/>
+						<SupportTile
+							image={documentationUrl}
+							title="Cloud Documentation"
+							onClick={() => adapter.openUrl("https://surrealdb.com/docs/cloud")}
+						/>
+						<SupportTile
+							image={communtyUrl}
+							title="Community Forums"
+							onClick={() =>
+								adapter.openUrl("https://surrealdb.com/community/forums")
+							}
+						/>
 					</Stack>
 					{/* <Divider
 						orientation="vertical"
