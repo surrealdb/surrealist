b783e984b01a5581da149c832ce3889fe2c87e10
Julian Mills <julian.exodius@gmail.com>
2024-09-13 16:18:05 +0200

    update url

--- src/screens/cloud-manage/api/endpoints.tsx
+++ src/screens/cloud-manage/api/endpoints.tsx
@@ -1,13 +1,12 @@
 import { useConfigStore } from "~/stores/config";
 import { featureFlags } from "~/util/feature-flags";
 
-const CLOUD_AUTH_BASE = "https://surrealdb.us.auth0.com";
+const CLOUD_AUTH_BASE = "https://auth.surrealdb.com";
 const CLOUD_API_BASE = "https://api.cloud.surrealdb.com/api/v1";
 const CLOUD_API_MGMT_BASE = "https://api.cloud.surrealdb.com/management/v1";
 
 export function getCloudEndpoints() {
-	const { urlAuthBase, urlApiBase, urlApiMgmtBase } =
-		useConfigStore.getState().settings.cloud;
+	const { urlAuthBase, urlApiBase, urlApiMgmtBase } = useConfigStore.getState().settings.cloud;
 	const isCustom = featureFlags.get("cloud_endpoints") === "custom";
 
 	return {
