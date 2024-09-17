2fb2435d3d0fca2494ec7f3afed789d05f5285cc
Julian Mills <julian.exodius@gmail.com>
2024-09-17 16:14:58 +0100

    fmt

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     tauri_build::build();
 
-    if let Err(_) = std::env::var("VITE_SURREALIST_PREVIEW") {
+    if std::env::var("VITE_SURREALIST_PREVIEW").is_err() {
         println!("cargo:rustc-env=VITE_SURREALIST_PREVIEW=false");
     }
 }
