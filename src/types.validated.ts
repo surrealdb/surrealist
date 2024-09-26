5ffe2f8805df80f487caff4bcff64373dcbad39e
Odonno <bottiau.david@laposte.net>
2024-09-26 17:49:46 +0200

    feat: improve type validation of protocol/auth mode

--- src/types.validated.ts
+++ src/types.validated.ts
@@ -1,4 +1,20 @@
 import * as v from "valibot";
+import type { AuthMode, Protocol } from "./types";
+
+const createAuthModeSchema = () => {
+	// only "root" mode to be used as of now
+	const authModes: AuthMode[] = ["none", "root"];
+	return v.union(authModes.map((mode) => v.literal(mode)));
+};
+
+const createProtocolSchema = () => {
+	// "mem" protocol not needed in embedded config
+	const protocols: Protocol[] = ["http", "https", "ws", "wss", "indxdb"];
+	return v.union(protocols.map((protocol) => v.literal(protocol)));
+};
+
+const AuthModeSchema = createAuthModeSchema();
+const ProtocolSchema = createProtocolSchema();
 
 export const SurrealistEmbeddedConfigSchema = v.object({
 	activeConnection: v.optional(v.string()),
@@ -8,9 +24,9 @@ export const SurrealistEmbeddedConfigSchema = v.object({
 				id: v.string(),
 				name: v.string(),
 				authentication: v.object({
-					protocol: v.string(),
+					protocol: ProtocolSchema,
 					hostname: v.string(),
-					mode: v.string(),
+					mode: AuthModeSchema,
 					username: v.string(),
 					password: v.optional(v.string()),
 					namespace: v.optional(v.string()),
