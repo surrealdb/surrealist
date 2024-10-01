7dc2567ced77c11f73c28ccecff05c478017764d
Odonno <bottiau.david@laposte.net>
2024-10-01 22:13:22 +0200

    feat: save config in indexeddb

--- src/util/storage.tsx
+++ src/util/storage.tsx
@@ -3,3 +3,4 @@ export const STATE_RES_KEY = "surrealist:oauth_response_state";
 export const VERIFIER_KEY = "surrealist:oauth_verifier";
 export const REFRESH_TOKEN_KEY = "surrealist:oauth_refresh_token";
 export const STATE_KEY = "surrealist:oauth_state";
+export const CONFIG_KEY = "surrealist:config";
