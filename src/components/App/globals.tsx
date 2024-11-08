ea0652a03e3e3e7589010ac90027f2053601659e
Julian Mills <julian.exodius@gmail.com>
2024-11-08 16:22:23 +0100

    refactor keybind logic into dedicated hook

--- src/components/App/globals.tsx
+++ src/components/App/globals.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 import { useCloudAuthentication } from "~/hooks/cloud";
 import { useConnectionSwitch } from "./hooks/connection";
-import { useModKeyTracker } from "./hooks/input";
+import { useKeybindListener, useModKeyTracker } from "./hooks/input";
 import { useConfigRouting } from "./hooks/routing";
 import { useTitleSync } from "./hooks/title";
 import { useWindowSettings } from "./hooks/window";
@@ -9,6 +9,7 @@ import { useWindowSettings } from "./hooks/window";
 export function Globals(): ReactNode {
 	useConfigRouting();
 	useModKeyTracker();
+	useKeybindListener();
 	useWindowSettings();
 	useConnectionSwitch();
 	useCloudAuthentication();
