07aaacb54936723e2a92a49d4a93236155030041
Julian Mills <julian.exodius@gmail.com>
2024-11-13 20:16:43 +0100

    progress

--- src/util/markdown.tsx
+++ src/util/markdown.tsx
@@ -2,13 +2,25 @@ import { marked } from "marked";
 import { renderHighlighting } from "./surrealql";
 import { useInterfaceStore } from "~/stores/interface";
 
+// const LINK_PATTERN = /^(.+?)\|(\d+)$/;
+
 marked.use({
 	gfm: true,
 	renderer: {
-		code({ text, lang }) {
+		code({ text }) {
 			const { colorScheme } = useInterfaceStore.getState();
 
 			return renderHighlighting(text, colorScheme);
 		},
+		// link({ href, title, text }) {
+		// 	const [full, clean, source] = href.match(LINK_PATTERN) ?? [];
+
+		// 	if (full) {
+		// 		const isSame = href === text;
+		// 		return `<a href="${clean}" title="${title}" target="_blank">${isSame ? clean : text}</a><sup>${source}</sup>`;
+		// 	}
+
+		// 	return `<a href="${href}" title="${title}" target="_blank">${text}</a>`;
+		// },
 	},
 });
