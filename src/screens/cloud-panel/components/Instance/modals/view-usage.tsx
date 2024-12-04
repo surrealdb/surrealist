1d019f73179e92f12c27266b95a22c4614d489f1
Julian Mills <julian.exodius@gmail.com>
2024-12-04 19:14:47 +0100

    update usage

--- src/screens/cloud-panel/components/Instance/modals/view-usage.tsx
+++ src/screens/cloud-panel/components/Instance/modals/view-usage.tsx
@@ -1,4 +1,14 @@
-import { Box, Divider, Group, Loader, Paper, Skeleton, Text, ThemeIcon } from "@mantine/core";
+import {
+	Box,
+	Divider,
+	Group,
+	Loader,
+	Paper,
+	Progress,
+	Skeleton,
+	Text,
+	ThemeIcon,
+} from "@mantine/core";
 import { Stack } from "@mantine/core";
 import { openModal } from "@mantine/modals";
 import { useMemo } from "react";
@@ -7,7 +17,7 @@ import { Label } from "~/components/Label";
 import { PrimaryTitle } from "~/components/PrimaryTitle";
 import { useCloudUsageQuery } from "~/screens/cloud-panel/hooks/usage";
 import type { CloudInstance } from "~/types";
-import { iconQuery } from "~/util/icons";
+import { iconDatabase, iconQuery } from "~/util/icons";
 
 export async function openUsageModal(instance: CloudInstance) {
 	openModal({
@@ -46,6 +56,51 @@ function InstanceUsageModal({ instance }: InstanceUsageModalProps) {
 
 	return (
 		<Stack>
+			{/* <Paper
+				bg="slate.9"
+				p="xl"
+				style={{ userSelect: "text", WebkitUserSelect: "text" }}
+			>
+				<Group>
+					<ThemeIcon
+						size="xl"
+						color="blue"
+						variant="light"
+					>
+						<Icon
+							path={iconDatabase}
+							size="lg"
+						/>
+					</ThemeIcon>
+					<Box flex={1}>
+						<Text
+							c="bright"
+							fw={500}
+							fz="lg"
+						>
+							Storage usage
+						</Text>
+						<Text fz="sm">Current disk utilization</Text>
+					</Box>
+					<Text
+						c="bright"
+						fw={700}
+						fz={20}
+					>
+						0 MB
+					</Text>
+				</Group>
+
+				<Progress
+					mt="xl"
+					value={30}
+					color="blue"
+					size="sm"
+				/>
+
+				<Text mt="sm">You have used 30% of your 4 GB storage limit</Text>
+			</Paper> */}
+
 			<Paper
 				bg="slate.9"
 				p="xl"
@@ -81,21 +136,21 @@ function InstanceUsageModal({ instance }: InstanceUsageModalProps) {
 						<Text
 							c="bright"
 							fw={700}
-							fz={22}
+							fz={20}
 						>
 							{totalCompute}h
 						</Text>
 					)}
 				</Group>
 
-				{computeHistory.length > 0 && (
+				{computeHistory.length > 1 && (
 					<>
 						<Divider
 							my="xl"
 							color="slate.6"
 						/>
 
-						<Label>Instance types</Label>
+						<Label>Instance type breakdown</Label>
 
 						{computeHistory.map(({ compute_hours, instance_type }) => (
 							<Group key={instance_type}>
@@ -119,42 +174,6 @@ function InstanceUsageModal({ instance }: InstanceUsageModalProps) {
 					</>
 				)}
 			</Paper>
-
-			{/* <Paper
-				bg="slate.9"
-				p="xl"
-				style={{ userSelect: "text", WebkitUserSelect: "text" }}
-			>
-				<Group>
-					<ThemeIcon
-						size="xl"
-						color="blue"
-						variant="light"
-					>
-						<Icon
-							path={iconDatabase}
-							size="lg"
-						/>
-					</ThemeIcon>
-					<Box flex={1}>
-						<Text
-							c="bright"
-							fw={500}
-							fz="lg"
-						>
-							Storage usage
-						</Text>
-						<Text fz="sm">Current disk utilization</Text>
-					</Box>
-					<Text
-						c="bright"
-						fw={700}
-						fz={22}
-					>
-						0 MB
-					</Text>
-				</Group>
-			</Paper> */}
 		</Stack>
 	);
 }
