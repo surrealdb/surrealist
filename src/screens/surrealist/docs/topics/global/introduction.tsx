3c45aa885ad9ed3fd84afa04bb3a76312312bcd3
ekwuno <Obinnacodes@gmail.com>
2025-02-04 12:47:01 +0000

    enable java and Go view

--- src/screens/surrealist/docs/topics/global/introduction.tsx
+++ src/screens/surrealist/docs/topics/global/introduction.tsx
@@ -88,7 +88,7 @@ export function DocsGlobalIntroduction() {
 					<DriverSelector
 						value={language}
 						onChange={setLanguage}
-						disabled={["java", "go", "c"]}
+						disabled={["c"]}
 						cols={{
 							xs: 1,
 							sm: 2,
