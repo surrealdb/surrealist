0e26b6f7d1798c6330f7ce36c2b8eabda7f8ae2c
msanchezdev <manuel@msanchez.dev>
2025-02-21 15:36:09 -0400

    Add multicursor support to the editor
    Allow adding new cursor to the editor when pressing Mod+Alt+Up/Down arrow keys.
    Mimicking the behavior of other editors like VSCode.

--- src/editor/commands.tsx
+++ src/editor/commands.tsx
@@ -1,4 +1,5 @@
 import { startCompletion } from "@codemirror/autocomplete";
+import { EditorSelection, SelectionRange } from "@codemirror/state";
 import type { Command, EditorView } from "@codemirror/view";
 
 /**
@@ -7,4 +8,59 @@ import type { Command, EditorView } from "@codemirror/view";
 export const suggestCompletions: Command = (view: EditorView) => {
 	setTimeout(() => startCompletion(view));
 	return false;
+};
+
+/**
+ * Mimicking other editors add a new cursor in the specified direction for
+ * each selection.
+ */
+function addCursorVertically(view: EditorView, offset: number) {
+	const { state } = view;
+	const { doc } = state;
+	const newRanges = [] as SelectionRange[];
+
+	for (const range of state.selection.ranges) {
+		const line = doc.lineAt(range.head);
+
+		const newLineNumber = line.number + offset;
+		if (newLineNumber < 1 || newLineNumber > doc.lines) {
+			continue;
+		}
+
+		const lineAnchor = range.anchor - line.from;
+		const lineHead = range.head - line.from;
+		const newLine = doc.line(newLineNumber);
+		const newAnchor =
+			newLine.from + Math.min(Math.max(lineAnchor, 0), newLine.length);
+		const newTo =
+			newLine.from + Math.min(Math.max(lineHead, 0), newLine.length);
+
+		newRanges.push(EditorSelection.range(newAnchor, newTo));
+	}
+
+	if (newRanges.length === 0) return false;
+
+	view.dispatch({
+		selection: EditorSelection.create(
+			[...state.selection.ranges, ...newRanges],
+			state.selection.mainIndex,
+		),
+		scrollIntoView: true,
+	});
+
+	return true;
+}
+
+/**
+ * Add cursor above the current line
+ */
+export const addCursorAbove: Command = (view: EditorView) => {
+	return addCursorVertically(view, -1);
+};
+
+/**
+ * Add cursor below the current line
+ */
+export const addCursorBelow: Command = (view: EditorView) => {
+	return addCursorVertically(view, 1);
 };
