f0866e361da04a9e9a8b6eb0acded21b3f98a685
knackstedt <knackstedt@outlook.com>
2025-02-23 19:55:57 -0500

    Use locale number format for rows counter

--- src/screens/surrealist/views/explorer/ExplorerPane/index.tsx
+++ src/screens/surrealist/views/explorer/ExplorerPane/index.tsx
@@ -252,7 +252,7 @@ export function ExplorerPane({ activeTable, onCreateRecord }: ExplorerPaneProps)
 							mr={-6}
 						/>
 						<Text lineClamp={1}>
-							{recordQuery.isLoading ? "loading..." : `${recordCount || "no"} rows`}
+							{recordQuery.isLoading ? "loading..." : `${recordCount.toLocaleString() || "no"} rows`}
 						</Text>
 					</Group>
 				)
