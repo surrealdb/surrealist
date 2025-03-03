2e7c41e41bf4d4549072c369b9b68abce4e21798
Julian Mills <julian.exodius@gmail.com>
2025-03-03 15:48:14 +0100

    add sidekick redirect

--- src/screens/surrealist/views/sidekick/SidekickView/index.tsx
+++ src/screens/surrealist/views/sidekick/SidekickView/index.tsx
@@ -1,11 +1,11 @@
-import { useViewFocus } from "~/hooks/routing";
+import { useAbsoluteLocation, useViewFocus } from "~/hooks/routing";
 
 export function SidekickView() {
-	// const [, setPage] = useActiveCloudPage();
+	const [, navigate] = useAbsoluteLocation();
 
 	// For now, redirect to the chat page when the sidekick is focused
 	useViewFocus("sidekick", () => {
-		// setPage("chat");
+		navigate("/chat");
 	});
 
 	return null;
