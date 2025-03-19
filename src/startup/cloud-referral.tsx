2268a25a8fdd667c4dc1c907f4d4969be092c31b
Julian Mills <julian.exodius@gmail.com>
2025-03-19 16:54:10 +0100

    fix desktop cloud auth

--- src/startup/cloud-referral.tsx
+++ src/startup/cloud-referral.tsx
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(location.search);
 
 if (params.has("code")) {
-	location.href = `${location.origin}/cloud?referrer=${params.get("code")}`;
+	location.href = `${location.origin}/referrals?referrer=${params.get("code")}`;
 } else {
-	location.href = `${location.origin}/cloud`;
+	location.href = `${location.origin}/referrals`;
 }
