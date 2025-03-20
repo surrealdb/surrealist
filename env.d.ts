d9e96331c098c8f7b458122a4caf72a6065d8653
Beau den Heijer <beaudenheijer@gmail.com>
2025-03-20 11:53:44 +0100

    make `import.meta.env` type safe

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	DATE: string;
+	VERSION: string;
+	SDB_VERSION: string;
+	MODE: string;
+	POSTHOG_KEY: string;
+	POSTHOG_URL: string;
+	GTM_ID: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
