00ec3332d63eaea411bbcc8cdd1ee48b1ead79f2
Julian Mills <julian.exodius@gmail.com>
2025-03-31 15:52:39 +0200

    share sidekick logic

--- src/screens/surrealist/views/sidekick/SidekickView/index.tsx
+++ src/screens/surrealist/views/sidekick/SidekickView/index.tsx
@@ -1,14 +1,7 @@
-import { useAbsoluteLocation, useViewFocus } from "~/hooks/routing";
+import { Sidekick } from "~/components/Sidekick";
 
 export function SidekickView() {
-	const [, navigate] = useAbsoluteLocation();
-
-	// For now, redirect to the chat page when the sidekick is focused
-	useViewFocus("sidekick", () => {
-		navigate("/chat");
-	});
-
-	return null;
+	return <Sidekick />;
 }
 
 export default SidekickView;
