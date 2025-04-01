dc6a21896d3545524903fd2ac998ee917c378efa
Julian Mills <julian.exodius@gmail.com>
2025-04-01 14:05:05 +0200

    progress

--- src/screens/surrealist/pages/CreateOrganization/index.tsx
+++ src/screens/surrealist/pages/CreateOrganization/index.tsx
@@ -1,14 +1,45 @@
 import classes from "./style.module.scss";
 
-import { Alert, Box, Button, ScrollArea, Stack, Text } from "@mantine/core";
+import { Alert, Box, Button, Group, ScrollArea, Stack, Text, TextInput } from "@mantine/core";
+import { useInputState } from "@mantine/hooks";
+import { useMutation } from "@tanstack/react-query";
 import { Link } from "wouter";
+import { fetchAPI } from "~/cloud/api";
 import { AuthGuard } from "~/components/AuthGuard";
 import { Icon } from "~/components/Icon";
 import { PrimaryTitle } from "~/components/PrimaryTitle";
+import { Spacer } from "~/components/Spacer";
 import { TopGlow } from "~/components/TopGlow";
+import { useAbsoluteLocation } from "~/hooks/routing";
+import { useStable } from "~/hooks/stable";
+import { CloudOrganization } from "~/types";
+import { tagEvent } from "~/util/analytics";
 import { iconArrowLeft } from "~/util/icons";
 
 export function CreateOrganizationPage() {
+	const [, navigate] = useAbsoluteLocation();
+	const [name, setName] = useInputState("");
+
+	const { mutateAsync } = useMutation({
+		mutationKey: ["create-organization"],
+		mutationFn: async () => {
+			const result = await fetchAPI<CloudOrganization>("/organizations", {
+				method: "POST",
+				body: JSON.stringify({ name }),
+			});
+
+			tagEvent("cloud_organization_created");
+
+			console.log(result);
+
+			navigate("/overview");
+		},
+	});
+
+	const handleCreate = useStable(async () => {
+		mutateAsync();
+	});
+
 	return (
 		<AuthGuard>
 			<Box
@@ -48,7 +79,45 @@ export function CreateOrganizationPage() {
 							</Button>
 						</Link>
 
-						<Alert title="Coming soon">This functionality is not yet available</Alert>
+						<Box mt="xl">
+							<Text
+								fz="xl"
+								fw={600}
+								c="bright"
+							>
+								Name
+							</Text>
+							<Text>Specify the name of your organization</Text>
+						</Box>
+
+						<TextInput
+							placeholder="My organization"
+							value={name}
+							onChange={setName}
+						/>
+
+						<Group mt="xl">
+							<Link to="/overview">
+								<Button
+									w={150}
+									color="slate"
+									variant="light"
+									leftSection={<Icon path={iconArrowLeft} />}
+								>
+									Back to overview
+								</Button>
+							</Link>
+							<Spacer />
+							<Button
+								w={150}
+								type="submit"
+								variant="gradient"
+								disabled={!name}
+								onClick={handleCreate}
+							>
+								Create organization
+							</Button>
+						</Group>
 					</Stack>
 				</ScrollArea>
 			</Box>
