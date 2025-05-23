f3b8d6e9acfee300779b459917231ec594d48295
Julian Mills <julian.exodius@gmail.com>
2025-05-23 21:07:36 +0200

    fix infinite store update loop

--- src/adapter/desktop.tsx
+++ src/adapter/desktop.tsx
@@ -1,6 +1,6 @@
 import { getHotkeyHandler } from "@mantine/hooks";
 import { invoke } from "@tauri-apps/api/core";
-import { listen } from "@tauri-apps/api/event";
+import { Event, listen } from "@tauri-apps/api/event";
 import { basename } from "@tauri-apps/api/path";
 import { getCurrentWebview } from "@tauri-apps/api/webview";
 import { getCurrentWindow } from "@tauri-apps/api/window";
@@ -17,7 +17,7 @@ import { ConfigStore, useConfigStore } from "~/stores/config";
 import { useDatabaseStore } from "~/stores/database";
 import { useInterfaceStore } from "~/stores/interface";
 import type { Platform, QueryTab, SurrealistConfig, ViewPage } from "~/types";
-import { getSetting, watchStore } from "~/util/config";
+import { getSetting, overwriteConfig, watchStore } from "~/util/config";
 import { getConnection } from "~/util/connection";
 import { featureFlags } from "~/util/feature-flags";
 import { NavigateViewEvent } from "~/util/global-events";
@@ -84,10 +84,8 @@ export class DesktopAdapter implements SurrealistAdapter {
 			this.queryOpenRequest();
 		});
 
-		listen("config-updated", (event) => {
-			const config = JSON.parse(event.payload as string);
-			useConfigStore.setState(config);
-			console.log("Config updated", config);
+		listen("config-updated", (event: Event<string>) => {
+			overwriteConfig(JSON.parse(event.payload));
 		});
 
 		listen("tauri://focus", () => {
@@ -131,7 +129,7 @@ export class DesktopAdapter implements SurrealistAdapter {
 	}
 
 	public async loadConfig() {
-		switch (await type()) {
+		switch (type()) {
 			case "windows": {
 				this.platform = "windows";
 				break;
