67406057445ad36f5d62b01a670bf528a97ba134
ItsMateo <me@itsmateo.dev>
2025-05-30 12:57:21 -0400

    feat: cloud auth sync and better config sync

--- src/adapter/desktop.tsx
+++ src/adapter/desktop.tsx
@@ -1,6 +1,6 @@
 import { getHotkeyHandler } from "@mantine/hooks";
 import { invoke } from "@tauri-apps/api/core";
-import { Event, listen } from "@tauri-apps/api/event";
+import { listen } from "@tauri-apps/api/event";
 import { basename } from "@tauri-apps/api/path";
 import { getCurrentWebview } from "@tauri-apps/api/webview";
 import { getCurrentWindow } from "@tauri-apps/api/window";
@@ -17,7 +17,7 @@ import { useConfigStore } from "~/stores/config";
 import { useDatabaseStore } from "~/stores/database";
 import { useInterfaceStore } from "~/stores/interface";
 import type { Platform, QueryTab, SurrealistConfig, ViewPage } from "~/types";
-import { getSetting, overwriteConfig, watchStore } from "~/util/config";
+import { getSetting, watchStore } from "~/util/config";
 import { getConnection } from "~/util/connection";
 import { featureFlags } from "~/util/feature-flags";
 import { NavigateViewEvent } from "~/util/global-events";
@@ -83,10 +83,6 @@ export class DesktopAdapter implements SurrealistAdapter {
 
 		getCurrentWindow().listen("open-resource", () => {
 			this.queryOpenRequest();
-		});
-
-		getCurrentWindow().listen("config-updated", (event: Event<string>) => {
-			overwriteConfig(JSON.parse(event.payload));
 		});
 
 		getCurrentWindow().listen("tauri://focus", () => {
