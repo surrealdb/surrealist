51fdb2ba3b86d4fff8d624e8d7a4501c3aba8400
ItsMateo <mmcfad443@gmail.com>
2025-06-06 11:50:51 -0400

    fmt

--- src-tauri/src/appbar.rs
+++ src-tauri/src/appbar.rs
@@ -3,10 +3,10 @@ pub mod macos {
     use cocoa::appkit::{NSApp, NSMenu, NSMenuItem};
     use cocoa::base::nil;
     use cocoa::foundation::NSString;
-    use objc::runtime::{Object, Sel, Class};
+    use objc::declare::ClassDecl;
+    use objc::runtime::{Class, Object, Sel};
     use objc::{msg_send, sel, sel_impl};
     use std::sync::Once;
-    use objc::declare::ClassDecl;
 
     pub fn setup_dock_menu() {
         static INIT: Once = Once::new();
@@ -16,24 +16,28 @@ pub mod macos {
             let dock_menu: *mut Object = NSMenu::new(nil);
             let title = NSString::alloc(nil).init_str("New Window");
             let selector = Sel::register("openNewWindowAction:");
-            let item: *mut Object = NSMenuItem::alloc(nil)
-                .initWithTitle_action_keyEquivalent_(
-                    title,
-                    selector,
-                    NSString::alloc(nil).init_str("")
-                );
+            let item: *mut Object = NSMenuItem::alloc(nil).initWithTitle_action_keyEquivalent_(
+                title,
+                selector,
+                NSString::alloc(nil).init_str("")
+            );
 
             let class_name = "SurrealistDockMenuTarget";
             let mut decl = ClassDecl::new(class_name, Class::get("NSObject").unwrap()).unwrap();
 
             extern "C" fn open_new_window_action(_this: &Object, _cmd: Sel, _sender: *mut Object) {
-                tauri::async_runtime::block_on(crate::window::open_new_window(crate::get_app_handle()));
+                tauri::async_runtime::block_on(crate::window::open_new_window(
+                    crate::get_app_handle(),
+                ));
             }
 
             let selector = Sel::register("openNewWindowAction:");
 
-            decl.add_method(selector, open_new_window_action as extern "C" fn(&Object, Sel, *mut Object));
-			
+            decl.add_method(
+                selector,
+                open_new_window_action as extern "C" fn(&Object, Sel, *mut Object),
+            );
+
             let custom_class = decl.register();
             let target: *mut Object = msg_send![custom_class, new];
 
