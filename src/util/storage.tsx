32efcd999330c7d7e715c30ca0414d2c8894083c
Julian Mills <julian.exodius@gmail.com>
2025-06-11 21:45:42 +0200

    improve cloud loading

--- src/util/storage.tsx
+++ src/util/storage.tsx
@@ -2,6 +2,7 @@ export const CODE_RES_KEY = "surrealist:oauth_response_code";
 export const STATE_RES_KEY = "surrealist:oauth_response_state";
 export const VERIFIER_KEY = "surrealist:oauth_verifier";
 export const REFRESH_TOKEN_KEY = "surrealist:oauth_refresh_token";
+export const ACCESS_TOKEN_KEY = "surrealist:oauth_access_token";
 export const REFERRER_KEY = "surrealist:referrer_code";
 export const INVITATION_KEY = "surrealist:invitation_code";
 export const STATE_KEY = "surrealist:oauth_state";
