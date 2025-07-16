a0a4e102689c0daa68d4777f51ce0302a88f46ac
Julian Mills <julian.exodius@gmail.com>
2025-07-16 14:55:05 +0200

    history panel

--- src/components/App/modals/sidekick.tsx
+++ src/components/App/modals/sidekick.tsx
@@ -15,6 +15,8 @@ import {
 	Image,
 	Paper,
 	ThemeIcon,
+	Transition,
+	Flex,
 } from "@mantine/core";
 
 import {
@@ -33,6 +35,7 @@ import {
 	iconQuery,
 	iconStar,
 	iconTransfer,
+	iconChat,
 } from "~/util/icons";
 
 import { Divider, Drawer } from "@mantine/core";
@@ -51,6 +54,7 @@ import { shuffle } from "radash";
 import { adapter } from "~/adapter";
 import { openCloudAuthentication } from "~/cloud/api/auth";
 import { Spacer } from "~/components/Spacer";
+import { CloudChatMessage } from "~/types";
 
 const QUESTIONS = [
 	{ icon: iconCreditCard, title: "How do I manage Cloud billing?" },
@@ -72,58 +76,15 @@ export function SidekickDrawer() {
 	const handleClose = useStable(() => {
 		openHandle.close();
 	});
+
+	const [width, setWidth] = useState(650);
+	const [showHistory, setShowHistory] = useState(false);
+	const isAuthed = useIsAuthenticated();
+	const conversation = useCloudStore((s) => s.chatConversation);
+
 	useIntent("open-sidekick", () => {
 		openHandle.open();
 	});
-	const [width, setWidth] = useState(650);
-
-	const { pushChatMessage } = useCloudStore.getState();
-	const inputRef = useRef<HTMLTextAreaElement>(null);
-	const scrollRef = useRef<HTMLDivElement>(null);
-	const [input, setInput] = useInputState("");
-	const profile = useCloudProfile();
-	const isAuthed = useIsAuthenticated();
-	const conversation = useCloudStore((s) => s.chatConversation);
-	const lastResponse = useCloudStore((s) => s.chatLastResponse);
-	const { sendMessage, isResponding } = useCopilotMutation();
-	const hasMessage = useMemo(() => input.trim() !== "", [input]);
-	const canSend = input && !isResponding && hasMessage;
-
-	const submitMessage = useStable(() => {
-		if (!canSend) return;
-		pushChatMessage({
-			id: Math.random().toString(36).slice(2),
-			content: input,
-			sender: "user",
-			thinking: "",
-			loading: false,
-		});
-		inputRef.current?.focus();
-		sendMessage(input);
-		setInput("");
-	});
-
-	const handleKeyDown = useStable((e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-		if (e.key === "Enter" && !e.shiftKey) {
-			e.preventDefault();
-			submitMessage();
-		}
-	});
-
-	// biome-ignore lint/correctness/useExhaustiveDependencies: Only want to scroll on conversation change
-	useEffect(() => {
-		if (scrollRef.current) {
-			const { scrollHeight, clientHeight, scrollTop } = scrollRef.current;
-			if (scrollHeight - clientHeight - scrollTop < 150) {
-				scrollRef.current?.scrollTo({
-					top: scrollHeight,
-					behavior: "smooth",
-				});
-			}
-		}
-	}, [conversation]);
-
-	const questions = useMemo(() => shuffle(QUESTIONS).slice(0, 4), []);
 
 	return (
 		<Drawer
@@ -170,284 +131,375 @@ export function SidekickDrawer() {
 					</Box>
 					<PrimaryTitle>Sidekick</PrimaryTitle>
 					<Spacer />
-					<Button
-						size="xs"
-						color="slate"
-						variant="light"
-						leftSection={<Icon path={iconHistory} />}
-						onClick={() => {
-							openHandle.close();
-						}}
-					>
-						History
-					</Button>
-					<Button
-						size="xs"
-						variant="gradient"
-						rightSection={<Icon path={iconPlus} />}
-						onClick={() => {
-							openHandle.close();
-						}}
-					>
-						New chat
-					</Button>
+					{isAuthed && (
+						<>
+							<Button
+								size="xs"
+								color="slate"
+								variant="light"
+								leftSection={<Icon path={showHistory ? iconChat : iconHistory} />}
+								onClick={() => {
+									setShowHistory((v) => !v);
+								}}
+							>
+								{showHistory ? "Show chat" : "Show history"}
+							</Button>
+							<Button
+								size="xs"
+								variant="gradient"
+								rightSection={<Icon path={iconPlus} />}
+								onClick={() => {
+									// openHandle.close();
+								}}
+							>
+								New chat
+							</Button>
+						</>
+					)}
 				</Group>
 				<Divider />
 				<Box
 					flex={1}
-					w="100%"
 					pos="relative"
-					style={{ minHeight: 0 }}
+					style={{ overflow: "hidden" }}
 				>
-					{conversation.length > 0 && isAuthed ? (
-						<ScrollArea
-							pos="absolute"
-							viewportRef={scrollRef}
-							inset={0}
-							h="100%"
-						>
-							<Stack
-								p={36}
-								pb={125}
-								gap={38}
+					<Transition
+						mounted={!showHistory}
+						transition="fade-up"
+					>
+						{(styles) => (
+							<Flex
+								inset={0}
+								flex={1}
+								pos="absolute"
+								direction="column"
+								style={styles}
 							>
-								{conversation.map((message, i) => (
-									<ChatMessage
-										message={message}
-										profile={profile}
-										lastResponse={lastResponse}
-										isResponding={isResponding}
-										isLight={false}
-										key={i}
-									/>
-								))}
-							</Stack>
-						</ScrollArea>
-					) : (
-						<Center h="90%">
-							<Stack
-								align="center"
-								gap={0}
-							>
-								<Box pos="relative">
-									<Image
-										pos="absolute"
-										src={glowImg}
-										inset={0}
-										opacity={0.3}
-										style={{
-											transform: "scale(2)",
-											transition: "opacity 0.3s ease",
-										}}
-									/>
-									<Image
-										pos="relative"
-										src={sidekickImg}
-										w={132}
-										h={132}
-									/>
-								</Box>
-								<PrimaryTitle
-									fz={42}
-									mt="md"
-								>
-									Welcome to Sidekick
-								</PrimaryTitle>
-								<Text fz="xl">
-									Your personal Surreal assistant designed for you.
-								</Text>
-								<Stack
-									mt={36}
-									w={450}
-									style={{ alignSelf: "stretch" }}
-								>
-									{isAuthed ? (
-										questions.map((question) => (
-											<Paper
-												p="sm"
-												key={question.title}
-												role="button"
-												radius={100}
-												tabIndex={0}
-												variant="interactive"
-												style={{
-													alignSelf: "stretch",
-												}}
-												onClick={() => {
-													setInput(question.title);
-													inputRef.current?.focus();
-												}}
-											>
-												<Group
-													align="center"
-													wrap="nowrap"
-												>
-													<ThemeIcon
-														radius={100}
-														color="violet"
-														variant="light"
-														size="lg"
-													>
-														<Icon
-															path={question.icon}
-															size="md"
-														/>
-													</ThemeIcon>
-													<PrimaryTitle
-														c="bright"
-														fw={500}
-														fz="xl"
-														pr="md"
-													>
-														{question.title}
-													</PrimaryTitle>
-												</Group>
-											</Paper>
-										))
-									) : (
-										<Group
-											mt="xl"
-											w="100%"
-											maw={450}
-										>
-											<Button
-												flex={1}
-												variant="gradient"
-												onClick={openCloudAuthentication}
-												rightSection={<Icon path={iconChevronRight} />}
-											>
-												Sign in
-											</Button>
-											<Button
-												flex={1}
-												color="slate"
-												variant="light"
-												rightSection={<Icon path={iconOpen} />}
-												onClick={() =>
-													adapter.openUrl(
-														"https://surrealdb.com/sidekick",
-													)
-												}
-											>
-												Learn more
-											</Button>
-										</Group>
-									)}
-								</Stack>
-							</Stack>
-						</Center>
-					)}
-				</Box>
-				{isAuthed && (
-					<>
-						<Box
-							px="xl"
-							pb="xl"
-						>
+								<ChatConversation
+									conversation={conversation}
+									isAuthed={isAuthed}
+								/>
+							</Flex>
+						)}
+					</Transition>
+					<Transition
+						mounted={showHistory}
+						transition="fade-down"
+					>
+						{(styles) => (
 							<Paper
-								bg="slate.9"
-								p="md"
+								inset={0}
+								pos="absolute"
+								withBorder={false}
+								bg="var(--mantine-color-body)"
+								style={styles}
+								p="xl"
 							>
-								<Group mb="xs">
-									<Button
-										leftSection={<Icon path={iconTable} />}
-										variant="light"
-										color="slate"
-										size="xs"
-									>
-										Attach schema
-									</Button>
-									<Button
-										leftSection={<Icon path={iconTable} />}
-										variant="light"
-										color="slate"
-										size="xs"
-									>
-										Attach table
-									</Button>
-									<Button
-										leftSection={<Icon path={iconRelation} />}
-										variant="light"
-										color="slate"
-										size="xs"
-									>
-										Attach relation
-									</Button>
-									{/* <Spacer />
-									<Menu position="top">
-										<Menu.Target>
-											<ActionIcon
-												size="lg"
-												color="slate"
-												variant="subtle"
-											>
-												<Icon path={iconDotsVertical} />
-											</ActionIcon>
-										</Menu.Target>
-										<Menu.Dropdown>
-											<Menu.Item
-												leftSection={<Icon path={iconReset} />}
-												onClick={clearChatSession}
-											>
-												Clear chat
-											</Menu.Item>
-										</Menu.Dropdown>
-									</Menu> */}
-								</Group>
-								<Group
-									wrap="nowrap"
-									align="end"
-								>
-									<Textarea
-										ref={inputRef}
-										flex={1}
-										rows={1}
-										maxRows={12}
-										autosize
-										className={classes.sidekickInput}
-										placeholder="Send a message..."
-										onKeyDown={handleKeyDown}
-										value={input}
-										autoFocus
-										onChange={setInput}
-										rightSectionWidth={96}
-										variant="unstyled"
-									/>
-									<ActionIcon
-										size="lg"
-										type="submit"
-										variant="gradient"
-										disabled={!canSend}
-										onClick={submitMessage}
-										loading={isResponding}
-										style={{
-											opacity: canSend ? 1 : 0.5,
-											border: "1px solid rgba(255, 255, 255, 0.3)",
-											backgroundOrigin: "border-box",
-											filter: canSend ? undefined : "saturate(0%)",
-											transition: "all 0.1s",
-										}}
-									>
-										<Icon
-											path={iconCursor}
-											c="white"
-										/>
-									</ActionIcon>
-								</Group>
+								<ChatHistory conversation={conversation} />
 							</Paper>
-							{/* <Text
-								mt="sm"
-								ta="center"
-								c="slate"
-								fz="sm"
-							>
-								You are chatting with an AI assistant, please refrain from
-								submitting sensitive data.
-							</Text> */}
-						</Box>
-					</>
-				)}
+						)}
+					</Transition>
+				</Box>
 			</Stack>
 		</Drawer>
+	);
+}
+
+interface ChatConversationProps {
+	conversation: CloudChatMessage[];
+	isAuthed: boolean;
+}
+
+function ChatConversation({ conversation, isAuthed }: ChatConversationProps) {
+	const { pushChatMessage } = useCloudStore.getState();
+	const inputRef = useRef<HTMLTextAreaElement>(null);
+	const scrollRef = useRef<HTMLDivElement>(null);
+	const [input, setInput] = useInputState("");
+	const profile = useCloudProfile();
+	const lastResponse = useCloudStore((s) => s.chatLastResponse);
+	const { sendMessage, isResponding } = useCopilotMutation();
+	const hasMessage = useMemo(() => input.trim() !== "", [input]);
+	const canSend = input && !isResponding && hasMessage;
+
+	const submitMessage = useStable(() => {
+		if (!canSend) return;
+
+		if (!isAuthed) {
+			openCloudAuthentication();
+			return;
+		}
+
+		pushChatMessage({
+			id: Math.random().toString(36).slice(2),
+			content: input,
+			sender: "user",
+			thinking: "",
+			loading: false,
+		});
+
+		inputRef.current?.focus();
+		sendMessage(input);
+		setInput("");
+	});
+
+	const handleKeyDown = useStable((e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault();
+			submitMessage();
+		}
+	});
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: Only want to scroll on conversation change
+	useEffect(() => {
+		if (scrollRef.current) {
+			const { scrollHeight, clientHeight, scrollTop } = scrollRef.current;
+
+			if (scrollHeight - clientHeight - scrollTop < 150) {
+				scrollRef.current?.scrollTo({
+					top: scrollHeight,
+					behavior: "smooth",
+				});
+			}
+		}
+	}, [conversation]);
+
+	const questions = useMemo(() => shuffle(QUESTIONS).slice(0, 4), []);
+
+	return (
+		<>
+			{conversation.length > 0 ? (
+				<Box
+					flex={1}
+					pos="relative"
+				>
+					<ScrollArea
+						pos="absolute"
+						viewportRef={scrollRef}
+						inset={0}
+					>
+						<Stack
+							p={36}
+							pb={96}
+							gap={38}
+						>
+							{conversation.map((message, i) => (
+								<ChatMessage
+									message={message}
+									profile={profile}
+									lastResponse={lastResponse}
+									isResponding={isResponding}
+									isLight={false}
+									key={i}
+								/>
+							))}
+						</Stack>
+					</ScrollArea>
+				</Box>
+			) : (
+				<Center flex={1}>
+					<Stack
+						align="center"
+						gap={0}
+					>
+						<Box pos="relative">
+							<Image
+								pos="absolute"
+								src={glowImg}
+								inset={0}
+								opacity={0.3}
+								style={{
+									transform: "scale(2)",
+									transition: "opacity 0.3s ease",
+								}}
+							/>
+							<Image
+								pos="relative"
+								src={sidekickImg}
+								w={132}
+								h={132}
+							/>
+						</Box>
+						<PrimaryTitle
+							fz={42}
+							mt="md"
+						>
+							Welcome to Sidekick
+						</PrimaryTitle>
+						<Text fz="xl">Your personal Surreal assistant designed for you.</Text>
+						<Stack
+							mt={36}
+							w={450}
+							style={{ alignSelf: "stretch" }}
+						>
+							{isAuthed ? (
+								questions.map((question) => (
+									<Paper
+										p="sm"
+										key={question.title}
+										role="button"
+										radius={100}
+										tabIndex={0}
+										variant="interactive"
+										style={{
+											alignSelf: "stretch",
+										}}
+										onClick={() => {
+											setInput(question.title);
+											inputRef.current?.focus();
+										}}
+									>
+										<Group
+											align="center"
+											wrap="nowrap"
+										>
+											<ThemeIcon
+												radius={100}
+												color="violet"
+												variant="light"
+												size="lg"
+											>
+												<Icon
+													path={question.icon}
+													size="md"
+												/>
+											</ThemeIcon>
+											<PrimaryTitle
+												c="bright"
+												fw={500}
+												fz="xl"
+												pr="md"
+											>
+												{question.title}
+											</PrimaryTitle>
+										</Group>
+									</Paper>
+								))
+							) : (
+								<Group
+									w="100%"
+									maw={450}
+								>
+									<Button
+										flex={1}
+										variant="gradient"
+										onClick={openCloudAuthentication}
+										rightSection={<Icon path={iconChevronRight} />}
+									>
+										Sign in
+									</Button>
+									<Button
+										flex={1}
+										color="slate"
+										variant="light"
+										rightSection={<Icon path={iconOpen} />}
+										onClick={() =>
+											adapter.openUrl("https://surrealdb.com/sidekick")
+										}
+									>
+										Learn more
+									</Button>
+								</Group>
+							)}
+						</Stack>
+					</Stack>
+				</Center>
+			)}
+			<Box
+				px="xl"
+				pb="xl"
+			>
+				<Paper
+					bg="slate.9"
+					p="md"
+				>
+					<Group mb="xs">
+						<Button
+							leftSection={<Icon path={iconTable} />}
+							variant="light"
+							color="slate"
+							size="xs"
+						>
+							Attach schema
+						</Button>
+						<Button
+							leftSection={<Icon path={iconTable} />}
+							variant="light"
+							color="slate"
+							size="xs"
+						>
+							Attach table
+						</Button>
+						<Button
+							leftSection={<Icon path={iconRelation} />}
+							variant="light"
+							color="slate"
+							size="xs"
+						>
+							Attach relation
+						</Button>
+					</Group>
+					<Group
+						wrap="nowrap"
+						align="end"
+					>
+						<Textarea
+							ref={inputRef}
+							flex={1}
+							rows={1}
+							maxRows={12}
+							autosize
+							className={classes.sidekickInput}
+							placeholder="Send a message..."
+							onKeyDown={handleKeyDown}
+							value={input}
+							autoFocus
+							onChange={setInput}
+							rightSectionWidth={96}
+							variant="unstyled"
+						/>
+						<ActionIcon
+							size="lg"
+							type="submit"
+							variant="gradient"
+							disabled={!canSend}
+							onClick={submitMessage}
+							loading={isResponding}
+							style={{
+								opacity: canSend ? 1 : 0.5,
+								border: "1px solid rgba(255, 255, 255, 0.3)",
+								backgroundOrigin: "border-box",
+								filter: canSend ? undefined : "saturate(0%)",
+								transition: "all 0.1s",
+							}}
+						>
+							<Icon
+								path={iconCursor}
+								c="white"
+							/>
+						</ActionIcon>
+					</Group>
+				</Paper>
+			</Box>
+		</>
+	);
+}
+
+interface ChatHistoryProps {
+	conversation: CloudChatMessage[];
+}
+
+function ChatHistory({ conversation }: ChatHistoryProps) {
+	return (
+		<Stack>
+			{/* {conversation.map((message) => (
+				<ChatMessage
+					message={message}
+					profile={profile}
+					lastResponse={lastResponse}
+					isResponding={isResponding}
+					isLight={false}
+				/>
+			))} */}
+		</Stack>
 	);
 }
