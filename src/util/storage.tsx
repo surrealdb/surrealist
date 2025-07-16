3c95981bdce50a4d736e37032f8c6495ab0ea09b
Beau den Heijer <beaudenheijer@gmail.com>
2025-07-16 12:27:42 +0200

    remove unused local storage key

--- src/util/storage.tsx
+++ src/util/storage.tsx
@@ -7,6 +7,5 @@ export const REFERRER_KEY = "surrealist:referrer_code";
 export const INVITATION_KEY = "surrealist:invitation_code";
 export const STATE_KEY = "surrealist:oauth_state";
 export const CONFIG_KEY = "surrealist:config";
-export const CLIENT_KEY = "surrealist:client";
 export const OVERVIEW_KEY = "surrealist:overview";
 export const APPLY_DATASET_KEY = "surrealist:apply_dataset";
