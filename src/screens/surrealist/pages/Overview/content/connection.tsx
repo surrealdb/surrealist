a6bac203a9f317f28b45f3b54b7470f74ce48d92
Matthew McFadden <matthew.mcfadden@surrealdb.com>
2026-02-20 16:52:19 -0500

    Implement misc fixes (#1128)

--- src/screens/surrealist/pages/Overview/content/connection.tsx
+++ src/screens/surrealist/pages/Overview/content/connection.tsx
@@ -1,19 +1,35 @@
 import {
+	ActionIcon,
 	Anchor,
 	Badge,
 	BoxProps,
 	Group,
+	Menu,
 	Paper,
 	Stack,
 	Text,
 	ThemeIcon,
 	UnstyledButton,
 } from "@mantine/core";
-import { Icon, iconChevronRight, iconSurreal, Spacer } from "@surrealdb/ui";
+import {
+	Icon,
+	iconCopy,
+	iconDelete,
+	iconDotsVertical,
+	iconEdit,
+	iconSurreal,
+	Spacer,
+} from "@surrealdb/ui";
 import { PropsWithChildren, useRef } from "react";
 import { SANDBOX } from "~/constants";
 import { useStable } from "~/hooks/stable";
+import { openConnectionEditModal } from "~/modals/edit-connection";
+import { useConfirmation } from "~/providers/Confirmation";
+import { useConfigStore } from "~/stores/config";
 import { Connection } from "~/types";
+import { tagEvent } from "~/util/analytics";
+import { getConnectionVariant } from "~/util/connection";
+import { newId, ON_STOP_PROPAGATION } from "~/util/helpers";
 import { USER_ICONS } from "~/util/user-icons";
 
 export interface StartConnectionProps extends BoxProps {
@@ -27,6 +43,7 @@ export function StartConnection({
 	children,
 	...other
 }: PropsWithChildren<StartConnectionProps>) {
+	const { addConnection, removeConnection } = useConfigStore.getState();
 	const { protocol, hostname } = connection.authentication;
 
 	const containerRef = useRef<HTMLDivElement>(null);
@@ -36,6 +53,40 @@ export function StartConnection({
 
 	const handleConnect = useStable(() => {
 		onConnect(connection);
+	});
+
+	const handleEdit = useStable(() => {
+		openConnectionEditModal(connection);
+	});
+
+	const handleDuplicate = useStable(() => {
+		addConnection({
+			...connection,
+			lastNamespace: "",
+			lastDatabase: "",
+			id: newId(),
+		});
+
+		tagEvent("connection_duplicated", {
+			protocol: connection.authentication.protocol.toString(),
+			variant: getConnectionVariant(connection),
+			is_local: connection.authentication.hostname.includes("localhost"),
+		});
+	});
+
+	const handleDelete = useConfirmation({
+		title: "Remove connection",
+		message: "Are you sure you want to remove this connection?",
+		skippable: true,
+		onConfirm() {
+			removeConnection(connection.id);
+
+			tagEvent("connection_deleted", {
+				protocol: connection.authentication.protocol.toString(),
+				variant: getConnectionVariant(connection),
+				is_local: connection.authentication.hostname.includes("localhost"),
+			});
+		},
 	});
 
 	const labels = connection?.labels?.map((label, i) => (
@@ -64,49 +115,101 @@ export function StartConnection({
 				>
 					<Group
 						wrap="nowrap"
+						align="strech"
+						flex={1}
 						mt={-3}
 					>
-						<ThemeIcon
-							size="xl"
-							variant="light"
-						>
-							<Icon
-								size={isSandbox ? "lg" : "md"}
-								path={isSandbox ? iconSurreal : USER_ICONS[connection.icon]}
-							/>
-						</ThemeIcon>
-						<Stack gap={0}>
-							<Group gap="sm">
-								<Text
-									c="bright"
-									fw={600}
-									fz="xl"
-									truncate
-								>
-									{connection.name}
-								</Text>
-
-								{isManaged ? (
+						<Group wrap="nowrap">
+							<ThemeIcon
+								size="xl"
+								variant="light"
+							>
+								<Icon
+									size={isSandbox ? "lg" : "md"}
+									path={isSandbox ? iconSurreal : USER_ICONS[connection.icon]}
+								/>
+							</ThemeIcon>
+							<Stack gap={0}>
+								<Group gap="sm">
 									<Text
+										c="bright"
 										fw={600}
-										fz="xs"
-										variant="gradient"
+										fz="xl"
+										truncate
 									>
-										BUILT-IN
+										{connection.name}
 									</Text>
-								) : (
-									<Group gap="xs">{labels}</Group>
-								)}
-							</Group>
-							<Text truncate>
-								{isSandbox ? "Your personal offline playground" : target}
-							</Text>
-						</Stack>
-						<Spacer />
-						<Icon
-							c="dimmed"
-							path={iconChevronRight}
-						/>
+
+									{isManaged ? (
+										<Text
+											fw={600}
+											fz="xs"
+											variant="gradient"
+										>
+											BUILT-IN
+										</Text>
+									) : (
+										<Group gap="xs">{labels}</Group>
+									)}
+								</Group>
+								<Text truncate>
+									{isSandbox ? "Your personal offline playground" : target}
+								</Text>
+							</Stack>
+						</Group>
+						{!isManaged && (
+							<>
+								<Spacer />
+								{/** biome-ignore lint/a11y/noStaticElementInteractions: It's fine */}
+								<div
+									onClick={ON_STOP_PROPAGATION}
+									onKeyDown={ON_STOP_PROPAGATION}
+								>
+									<Menu
+										transitionProps={{
+											transition: "scale-y",
+										}}
+									>
+										<Menu.Target>
+											<ActionIcon
+												color="slate"
+												variant="subtle"
+												component="div"
+											>
+												<Icon path={iconDotsVertical} />
+											</ActionIcon>
+										</Menu.Target>
+										<Menu.Dropdown>
+											<Menu.Item
+												leftSection={<Icon path={iconEdit} />}
+												onClick={handleEdit}
+											>
+												Edit connection
+											</Menu.Item>
+											<Menu.Item
+												leftSection={<Icon path={iconCopy} />}
+												onClick={handleDuplicate}
+											>
+												Duplicate
+											</Menu.Item>
+											<Menu.Divider />
+											<Menu.Item
+												leftSection={
+													<Icon
+														path={iconDelete}
+														c="red"
+													/>
+												}
+												onClick={handleDelete}
+												c="red"
+											>
+												Delete
+											</Menu.Item>
+										</Menu.Dropdown>
+									</Menu>
+								</div>
+							</>
+						)}
 					</Group>
 				</Paper>
 			</Anchor>
--- src/screens/surrealist/pages/Overview/content/instance.tsx
+++ src/screens/surrealist/pages/Overview/content/instance.tsx
@@ -1,4 +1,5 @@
 import {
+	ActionIcon,
 	Anchor,
 	Badge,
 	BoxProps,
@@ -9,12 +10,14 @@ import {
 	ThemeIcon,
 	UnstyledButton,
 } from "@mantine/core";
-import { Icon, iconChevronRight, iconCloud, Spacer } from "@surrealdb/ui";
+import { Icon, iconCloud, iconDotsVertical, Spacer } from "@surrealdb/ui";
 import { PropsWithChildren, useMemo, useRef } from "react";
 import { Faint } from "~/components/Faint";
+import { InstanceActions } from "~/components/InstanceActions";
 import { useConnectionList } from "~/hooks/connection";
 import { useStable } from "~/hooks/stable";
 import { CloudInstance, CloudOrganization, CloudRegion } from "~/types";
+import { ON_STOP_PROPAGATION } from "~/util/helpers";
 import { USER_ICONS } from "~/util/user-icons";
 import { StateBadge } from "../badge";
 
@@ -71,44 +74,60 @@ export function StartInstance({
 					ref={containerRef}
 				>
 					<Group
-						gap="lg"
 						wrap="nowrap"
+						align="strech"
 						mt={-3}
 					>
-						<ThemeIcon
-							color="obsidian"
-							variant="light"
-							size="xl"
-						>
-							<Icon
-								size="md"
-								path={connection ? USER_ICONS[connection.icon] : iconCloud}
-							/>
-						</ThemeIcon>
-						<Stack gap="xs">
-							<Group>
-								<Text
-									c="bright"
-									fw={600}
-									fz="xl"
-								>
-									{instance.name}
-								</Text>
-								<StateBadge
-									size={10}
-									state={instance.state}
+						<Group gap="lg">
+							<ThemeIcon
+								color="obsidian"
+								variant="light"
+								size="xl"
+							>
+								<Icon
+									size="md"
+									path={connection ? USER_ICONS[connection.icon] : iconCloud}
 								/>
-							</Group>
-							<Text>SurrealDB {instance.version}</Text>
-							<Text size="sm">
-								{regions.find((r) => r.slug === instance.region)?.description}
-							</Text>
-						</Stack>
+							</ThemeIcon>
+							<Stack gap="xs">
+								<Group>
+									<Text
+										c="bright"
+										fw={600}
+										fz="xl"
+									>
+										{instance.name}
+									</Text>
+									<StateBadge
+										size={10}
+										state={instance.state}
+									/>
+								</Group>
+								<Text>SurrealDB {instance.version}</Text>
+								<Text size="sm">
+									{regions.find((r) => r.slug === instance.region)?.description}
+								</Text>
+							</Stack>
+						</Group>
 						<Spacer />
-						<Icon
-							c="dimmed"
-							path={iconChevronRight}
-						/>
+						{/* biome-ignore lint/a11y/noStaticElementInteractions: Stop event propagation */}
+						<div
+							onClick={ON_STOP_PROPAGATION}
+							onKeyDown={ON_STOP_PROPAGATION}
+						>
+							<InstanceActions
+								instance={instance}
+								organisation={organisation}
+							>
+								<ActionIcon
+									color="slate"
+									variant="subtle"
+									component="div"
+								>
+									<Icon path={iconDotsVertical} />
+								</ActionIcon>
+							</InstanceActions>
+						</div>
 					</Group>
 					<Group gap="xs">{labels}</Group>
 					<Faint containerRef={containerRef} />
