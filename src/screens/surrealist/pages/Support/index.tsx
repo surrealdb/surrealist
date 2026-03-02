d901c2f41f6917d59a62f20ce06d0d43aa03eb6c
Matthew McFadden <mmcfad443@gmail.com>
2026-03-02 16:05:40 -0500

    fix: support collection gap and radius

--- src/screens/surrealist/pages/Support/index.tsx
+++ src/screens/surrealist/pages/Support/index.tsx
@@ -189,7 +189,7 @@ export function SupportPage() {
 							<SimpleGrid
 								mt="md"
 								cols={{ base: 1, md: 2 }}
-								spacing="xl"
+								spacing="md"
 								mx="auto"
 							>
 								{collections
