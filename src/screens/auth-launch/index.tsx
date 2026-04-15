0b40dc58a4aed5dd10ce68259beff039c425764b
Julian Mills <julian.exodius@gmail.com>
2026-04-15 13:14:45 +0200

    improve launch screen

--- src/screens/auth-launch/index.tsx
+++ src/screens/auth-launch/index.tsx
@@ -1,5 +1,6 @@
-import { MantineProvider, Stack, Text } from "@mantine/core";
-import { useLayoutEffect } from "react";
+import { Anchor, Button, MantineProvider, Stack, Text } from "@mantine/core";
+import { Icon, iconOpen, iconSurreal, Spinner } from "@surrealdb/ui";
+import { useLayoutEffect, useMemo } from "react";
 import { useThemePreference } from "~/hooks/theme";
 import { SURREALIST_THEME } from "~/util/mantine";
 import classes from "./style.module.scss";
@@ -7,10 +8,14 @@ import classes from "./style.module.scss";
 export function AuthLaunchScreen() {
 	const colorScheme = useThemePreference();
 
-	useLayoutEffect(() => {
+	const launchUrl = useMemo(() => {
 		const params = new URLSearchParams(window.location.search);
-		location.href = `surrealist://callback/auth?${params.toString()}`;
+		return `surrealist://callback/auth?${params.toString()}`;
 	}, []);
+
+	useLayoutEffect(() => {
+		location.href = launchUrl;
+	}, [launchUrl]);
 
 	return (
 		<MantineProvider
@@ -22,10 +27,46 @@ export function AuthLaunchScreen() {
 				className={classes.root}
 				justify="center"
 				align="center"
-				gap="xl"
+				gap={0}
 			>
-				<Text fz="lg">Opening Surrealist...</Text>
-				<Text c="obsidian">You can close this page once the app has opened</Text>
+				<Icon
+					path={iconSurreal}
+					size={72}
+					c="bright"
+				/>
+				<Text
+					fz={22}
+					fw={600}
+					mt="xl"
+				>
+					Opening Surrealist...
+				</Text>
+				<Text
+					fz="md"
+					c="slate"
+					mt="xs"
+				>
+					You can close this page once the app has opened
+				</Text>
+				<Spinner
+					mt={28}
+					size="sm"
+					color="surreal"
+				/>
+				<Anchor
+					href={launchUrl}
+					mt={42}
+					underline="always"
+					c="slate"
+					fz="sm"
+				>
+					<Button
+						variant="light"
+						leftSection={<Icon path={iconOpen} />}
+					>
+						Open manually
+					</Button>
+				</Anchor>
 			</Stack>
 		</MantineProvider>
 	);
