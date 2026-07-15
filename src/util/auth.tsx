56b3f3925f217e538ad8f5b6a69445c0971bd980
Beau den Heijer <beaudenheijer@gmail.com>
2026-07-15 10:27:43 +0200

    apply linting

--- src/util/auth.tsx
+++ src/util/auth.tsx
@@ -1,7 +1,7 @@
 import { User } from "@auth0/auth0-react";
 
 /**
- * @param user 
+ * @param user
  * @returns Retrieves auth provider from user id
  */
 export function getAuthProvider(user?: User): string {
